3fc63dcd23910e031bae6ca5b573207b3edddb5c
Bob McWhirter <bob@fnokd.com>
2009-05-13 00:42:12 -0400

    Refactoring endpoints stuff to modules.

--- torquebox-gem/lib/torquebox/endpoints/base.rb
+++ torquebox-gem/lib/torquebox/endpoints/base.rb
@@ -3,49 +3,73 @@ require 'jboss/endpoints/security_metadata'
 
 import org.jboss.ruby.enterprise.endpoints.BaseEndpointRb
 
-module JBoss
-module Endpoints
+module TorqueBox
+  module Endpoints
+    module Base
 
-class BaseEndpoint < BaseEndpointRb
+      def self.included(target)
+        puts "included into #{target}"
+      end
+      
+      def self.target_namespace(ns=nil)
+        ( @target_namspace = ns ) if ( ns != nil )
+        @target_namspace ||= nil
+      end
+  
+      def self.port_name(pn=nil)
+        ( @port_name = pn ) if ( pn != nil )
+        @port_name ||= nil
+      end
 
-  def self.target_namespace(ns=nil)
-    ( @target_namspace = ns ) if ( ns != nil )
-    @target_namspace ||= nil
+      def self.security(&block) 
+        unless block.nil?
+          @security = SecurityMetaData.new( &block )
+        end
+        @security
+      end
+
+      def log=(logger)
+        @logger = logger
+      end
+      
+      def log
+        @logger
+      end
+
+      def request=(request)
+        @request = request
+      end
+      
+      def request
+        @request
+      end
+
+      def principal=(principal)
+        @principal = principal        
+      end
+      
+      def principal
+        @principal
+      end
+      
+      def response_creator=(response_creator)
+        @response_creator = response_creator 
+      end
+      
+      def response_creator
+        @response_creator
+      end
+
+      def create_response
+        rc = response_creator
+        unless ( rc.nil? )
+          return eval( rc )
+        end
+        nil
+      end
+      
+    end 
+    
   end
-
-  def self.port_name(pn=nil)
-    ( @port_name = pn ) if ( pn != nil )
-    @port_name ||= nil
-  end
-
-  def self.security(&block) 
-    unless block.nil?
-      @security = SecurityMetaData.new( &block )
-    end
-    @security
-  end
-
-  def log
-    self.getLogger()
-  end
-
-  def request
-    self.getRequest()
-  end
-
-  def principal
-    self.getPrincipal()
-  end
-
-  def create_response
-    rc = self.getResponseCreator()
-    unless ( rc.nil? )
-      return eval rc
-    end
-    nil
-  end
-
 end
-
-end # Endpoints
-end # JBoss
+  
