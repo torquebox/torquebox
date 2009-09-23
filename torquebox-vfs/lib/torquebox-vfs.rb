3decc2932df4002cae6a5f8377a8b11f26c49fbd
Bob McWhirter <bob@fnokd.com>
2009-09-23 16:50:06 -0400

    Refactor finding the deepest real file. Break code into class-per-file some.

--- torquebox-vfs/lib/torquebox-vfs.rb
+++ torquebox-vfs/lib/torquebox-vfs.rb
@@ -2,6 +2,7 @@
 require 'java'
 require 'torquebox-vfs/vfs'
 require 'torquebox-vfs/ext/dir'
+require 'torquebox-vfs/glob_filter'
 #require 'torquebox-vfs/ext/io'
 #require 'torquebox-vfs/ext/file'
 #require 'torquebox-vfs/dir'
--- torquebox-vfs/lib/torquebox-vfs/vfs.rb
+++ torquebox-vfs/lib/torquebox-vfs/vfs.rb
@@ -8,3 +8,19 @@ class Java::OrgJbossVirtual::VFS
 
 end
 
+module VFS
+  def self.first_existing(path)
+    cur = path
+    while ( cur != '.' && cur != '/' )
+      if ( File.exist?( cur ) )
+        if ( cur[-1,1] == '/' )
+          cur = cur[0..-2]
+        end
+        return cur
+      end
+      cur = File.dirname( cur )
+    end
+    nil
+  end
+end
+
