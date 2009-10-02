f6d5fea83eafea0da88394ce9298f305b502deaa
Bob McWhirter <bob@fnokd.com>
2009-10-02 17:22:39 -0400

    Adding bundling-related Rake tasks.

--- torquebox-rails/lib/torquebox/tasks/torquebox_rails.rb
+++ torquebox-rails/lib/torquebox/tasks/torquebox_rails.rb
@@ -16,6 +16,58 @@ namespace :torquebox do
       JBoss::RakeUtils.undeploy( app_name )
       puts "Undeployed #{app_name}"
     end
+ 
+    namespace :deploy do
+      desc "Deploy the bundled Rails app"
+      task :bundle=>[ 'torquebox:rails:bundle' ] do
+        bundle_name = File.basename( RAILS_ROOT ) + '.jar'
+        src  = "#{RAILS_ROOT}/#{bundle_name}"
+        dest = "#{ENV['JBOSS_HOME']}/server/default/deploy/"
+        puts "deploying #{bundle_name}"
+        FileUtils.cp( src, dest )
+      end
+    end
+
+    desc "Bundle the Rails app"
+    task :bundle do
+      puts "Bundling #{RAILS_ROOT}"
+      bundle_name = File.basename( RAILS_ROOT ) + '.jar'
+      FileUtils.rm_rf( bundle_name )
+  
+      skip_files = [
+        'log',
+        'tmp',
+      ]
+      include_files = []
+      Dir[ "#{RAILS_ROOT}/*" ].each do |entry|
+        entry = File.basename( entry )
+        unless ( skip_files.include?( entry ) )
+          include_files << entry
+        end
+      end
+    
+      puts "building bundle #{bundle_name}"
+    
+      Dir.chdir( RAILS_ROOT ) do
+        cmd = "jar cf #{bundle_name} #{include_files.join(' ')}"
+        Open3.popen3( cmd ) do |stdin, stdout, stderr|
+          stdin.close
+          stdout_thr = Thread.new(stdout) {|stdout_io|
+            stdout_io.readlines.each do |l|
+              puts l
+            end
+            stdout_io.close
+          }
+          stderr_thr = Thread.new(stderr) {|stderr_io|
+            stderr_io.readlines.each do |l|
+              puts l
+            end
+          }
+          stdout_thr.join
+          stderr_thr.join
+        end
+      end
+    end
   end
 end
 
