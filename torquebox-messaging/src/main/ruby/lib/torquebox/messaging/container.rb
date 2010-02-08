5556907672e67785e500962ec8df82c1cc05573f
Bob McWhirter <bob@fnokd.com>
2010-02-08 17:04:03 -0500

    Complete dependencies to allow client to boot up.

--- torquebox-messaging/src/main/ruby/lib/torquebox/messaging/container.rb
+++ torquebox-messaging/src/main/ruby/lib/torquebox/messaging/container.rb
@@ -1,7 +1,7 @@
 
 container_factory = Java::org.torquebox.enterprise.ruby.messaging.container.ContainerFactory.new
 
-container_factory.connection_factory_jndi_name = "foo"
+container_factory.connection_factory_jndi_name = "/ConnectionFactory"
 
 
 puts "container_factory=#{container_factory}"
@@ -9,4 +9,11 @@ puts "container_factory=#{container_factory}"
 container = container_factory.create_container
 
 puts "container=#{container}"
+
+container.create
+container.start
+
+sleep( 5 )
+
+puts "exiting"
 
