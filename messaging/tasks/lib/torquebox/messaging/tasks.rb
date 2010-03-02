e5e2e7b0852b46a0a7c924f9014a6e25415d7e76
Bob McWhirter <bob@fnokd.com>
2010-03-02 16:46:42 -0500

    Adjusting client API for tasks.

--- messaging/tasks/lib/torquebox/messaging/tasks.rb
+++ messaging/tasks/lib/torquebox/messaging/tasks.rb
@@ -3,17 +3,47 @@ require 'torquebox-messaging-client'
 module TorqueBox
   module Messaging
     module Tasks
-      def self.enqueue(task_spec, payload={})
-        if ( task_spec =~ /^([^#]+)(#(.*))?$/ ) 
-          queue_base = $1
-          task_name  = $3 || 'execute'
-          TorqueBox::Messaging::Client.connect do |client|
-            client.send( "/queues/torquebox/tasks/#{queue_base}", 
-                         :task=>task_name,
-                         :payload=>payload )
+
+
+      def self.enqueue(task_spec, payload, connect_opts=nil)
+        TaskClient.connect( connect_opts ) do |client|
+          client.enqueue( task_spec, payload )
+        end 
+      end
+
+      class TaskClient
+
+        def self.connect(connect_opts=nil,&block)
+          Tasks.new( connect_opts, &block )
+        end
+
+        def initialize(connect_opts=nil,&block)
+          @client = TorqueBox::Messaging::Client.connect( connect_opts ) 
+          if ( block ) 
+            begin
+              instance_eval &block
+            ensure
+              @client.close
+            end
           end
         end
+
+        def close
+          @client.close
+        end
+
+        def enqueue(task_spec, payload={})
+          if ( task_spec =~ /^([^#]+)(#(.*))?$/ ) 
+            queue_base = $1
+            task_name  = $3 || 'execute'
+          end
+  
+          @client.send( "/queues/torquebox/tasks/#{queue_base}", 
+                       :task=>task_name,
+                       :payload=>payload )
+        end
       end
+
     end
   end
 end
