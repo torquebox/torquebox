4aa2640f12fa8c4348a9236c962aaa1cd4de9e61
Bob McWhirter <bob@fnokd.com>
2010-03-24 14:05:35 -0400

    Fix for TORQUE-72.  Check that object is loaded and non-NULL from queues.yml.

--- core/src/main/java/org/torquebox/messaging/deployers/QueuesYamlParsingDeployer.java
+++ core/src/main/java/org/torquebox/messaging/deployers/QueuesYamlParsingDeployer.java
@@ -10,36 +10,43 @@ import org.torquebox.messaging.metadata.QueueMetaData;
 import org.torquebox.messaging.metadata.QueuesMetaData;
 import org.yaml.snakeyaml.Yaml;
 
-public class QueuesYamlParsingDeployer extends AbstractVFSParsingDeployer<QueuesMetaData>{
+public class QueuesYamlParsingDeployer extends
+		AbstractVFSParsingDeployer<QueuesMetaData> {
 
 	public QueuesYamlParsingDeployer() {
-		super( QueuesMetaData.class );
-		setName( "queues.yml" );
+		super(QueuesMetaData.class);
+		setName("queues.yml");
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected QueuesMetaData parse(VFSDeploymentUnit unit, VirtualFile file, QueuesMetaData root) throws Exception {
+	protected QueuesMetaData parse(VFSDeploymentUnit unit, VirtualFile file,
+			QueuesMetaData root) throws Exception {
 		InputStream in = null;
-		
-		QueuesMetaData queues = unit.getAttachment( QueuesMetaData.class );
-		if ( queues == null ) {
+
+		QueuesMetaData queues = unit.getAttachment(QueuesMetaData.class);
+		if (queues == null) {
 			queues = new QueuesMetaData();
-			unit.addAttachment( QueuesMetaData.class, queues );
+			unit.addAttachment(QueuesMetaData.class, queues);
 		}
-		
+
 		try {
 			in = file.openStream();
 			Yaml yaml = new Yaml();
-			Map<String, Map<String,Object>> data = (Map<String, Map<String, Object>>) yaml.load( in );
-			
-			for ( String queueName : data.keySet() ) {
-				log.info( "Read configuration for queue [" + queueName + "]" );
-				QueueMetaData queue = new QueueMetaData( queueName );
-				queues.addQueue( queue );
+			Map<String, Map<String, Object>> data = (Map<String, Map<String, Object>>) yaml
+					.load(in);
+
+			if (data != null) {
+				for (String queueName : data.keySet()) {
+					log
+							.info("Read configuration for queue [" + queueName
+									+ "]");
+					QueueMetaData queue = new QueueMetaData(queueName);
+					queues.addQueue(queue);
+				}
 			}
 		} finally {
-			if ( in != null ) {
+			if (in != null) {
 				in.close();
 			}
 		}
