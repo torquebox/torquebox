71e842ede2fb1534ca8073307f92a6db75736e53
Bob McWhirter <bob@fnokd.com>
2010-05-24 12:22:08 -0400

    Rejig build to make each jboss-beans.xml uniquely named, and non-effectually placed in the integration jar.  Assembly also includes in within META-INF for effectivity.

--- components/messaging/messaging-int/src/test/java/org/torquebox/messaging/deployers/DeployersTest.java
+++ components/messaging/messaging-int/src/test/java/org/torquebox/messaging/deployers/DeployersTest.java
@@ -3,6 +3,7 @@ package org.torquebox.messaging.deployers;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.net.URL;
 
 import org.junit.Test;
 import org.torquebox.test.mc.vdf.AbstractDeployerTestCase;
@@ -11,7 +12,8 @@ public class DeployersTest extends AbstractDeployerTestCase {
 	
 	@Test
 	public void testJBossBeansXml() throws Exception {
-		File jbossBeansXml = new File( new File( new File( new File( new File( System.getProperty( "user.dir" ) , "src" ), "assembly" ), "resources" ), "META-INF" ), "jboss-beans.xml" );
+		//File jbossBeansXml = new File( new File( new File( new File( new File( System.getProperty( "user.dir" ) , "src" ), "assembly" ), "resources" ), "META-INF" ), "torquebox-messaging-int-jboss-beans.xml" );
+		URL jbossBeansXml = getClass().getResource( "/torquebox-messaging-int-jboss-beans.xml" );
 		
 		String deploymentName = addDeployment( jbossBeansXml );
 		
