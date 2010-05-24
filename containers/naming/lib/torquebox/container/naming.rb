593e54aeecfa10010a63aae5fffa149d8ebced48
Bob McWhirter <bob@fnokd.com>
2010-05-24 17:42:20 -0400

    Rename bin/ member; 'trq' seems cooler than 'tx', and less ambiguious (tx == transaction, sometimes). Manage the JNP server's Executor via MC beans.

--- containers/naming/lib/torquebox/container/naming.rb
+++ containers/naming/lib/torquebox/container/naming.rb
@@ -14,18 +14,11 @@ module TorqueBox
       def start
         super
         beans_xml = File.join( File.dirname(__FILE__), 'naming-jboss-beans.xml' )
-        puts "beans_xml=#{beans_xml}"
         @core_deployers = deploy( beans_xml )
         process_deployments(true)
-        puts "naming is #{self['Naming']}"
       end
 
       def stop
-        jndi = self['JNDIServer']
-        puts "naming is #{jndi}"
-        executor = jndi.getLookupExector()
-        puts "executor is #{executor}"
-        executor.shutdown
         undeploy( @core_deployers )
         process_deployments(true)
         super
