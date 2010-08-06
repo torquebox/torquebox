243f3d96d789fd74dd8ad8bac104664a22632075
jcrossley <jim@crossleys.org>
2010-08-06 16:16:16 -0400

    Bind tested naming service to an available port.
    Prevents port conflicts with JBoss if it happens to be running at the
    time you run your tests.  The available_port method isn't ideal -- I'd
    prefer to just set the naming service (and rmi) ports to 0, but it
    wasn't obvious to me how to query them for the actual ports returned
    by the OS.  Regardless, the tests pass with jboss running now. :)

--- containers/naming/spec/naming_service_spec.rb
+++ containers/naming/spec/naming_service_spec.rb
@@ -11,7 +11,10 @@ describe TorqueBox::Naming::NamingService do
   describe 'basics' do
     before(:each) do
       @container = TorqueBox::Container::Foundation.new
-      @container.enable( TorqueBox::Naming::NamingService )
+      @service = TorqueBox::Naming::NamingService.new do |config|
+        config.port, config.rmi_port = available_port(2)
+      end
+      @container.enable( @service )
       @container.start
     end
 
@@ -29,9 +32,9 @@ describe TorqueBox::Naming::NamingService do
       jndi_bean.should_not be_nil
     end
  
-    it "should be reachable on port 1099" do
+    it "should be reachable on port" do
      socket = Socket.new( Socket::Constants::AF_INET, Socket::Constants::SOCK_STREAM, 0 )
-     sockaddr = Socket.pack_sockaddr_in( 1099, 'localhost' )
+     sockaddr = Socket.pack_sockaddr_in( @service.port, 'localhost' )
      socket.connect( sockaddr )
      socket.close
     end
@@ -42,7 +45,7 @@ describe TorqueBox::Naming::NamingService do
     end
  
     it "should provide explicit connected bindability" do
-      TorqueBox::Naming.connect( 'localhost', 1099 ) do |context|
+      TorqueBox::Naming.connect( 'localhost', @service.port ) do |context|
         context['test'] = 'gouda'
         context['test'].should eql( 'gouda' )
       end
@@ -52,9 +55,11 @@ describe TorqueBox::Naming::NamingService do
   describe 'when not exported' do
     before(:each) do
       @container = TorqueBox::Container::Foundation.new
-      @container.enable( TorqueBox::Naming::NamingService ) do |config|
+      @service = TorqueBox::Naming::NamingService.new do |config|
+        config.port, config.rmi_port = available_port(2)
         config.export = false
       end
+      @container.enable( @service )
       @container.start
     end
 
@@ -72,9 +77,9 @@ describe TorqueBox::Naming::NamingService do
       jndi_bean.should be_nil
     end
 
-    it "should not be reachable on port 1099" do
+    it "should not be reachable on port" do
      socket = Socket.new( Socket::Constants::AF_INET, Socket::Constants::SOCK_STREAM, 0 )
-     sockaddr = Socket.pack_sockaddr_in( 1099, 'localhost' )
+     sockaddr = Socket.pack_sockaddr_in( @service.port, 'localhost' )
      lambda{ socket.connect( sockaddr ) }.should raise_error
     end
 
@@ -85,5 +90,11 @@ describe TorqueBox::Naming::NamingService do
  
   end
 
+end
 
+def available_port count=1
+  servers = (0...count).map{ TCPServer.new('127.0.0.1', 0)} 
+  ports = servers.map {|s| s.addr[1]}
+  servers.each {|s| s.close}
+  count==1 ? ports.first : ports
 end
