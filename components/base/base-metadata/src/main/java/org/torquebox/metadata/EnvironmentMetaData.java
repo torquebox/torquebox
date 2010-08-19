2fd2846f11094967b262960799162cdc0566fdc6
Bob McWhirter <bob@fnokd.com>
2010-08-19 12:10:47 -0400

    Add EnvMetaData.

---
+++ components/base/base-metadata/src/main/java/org/torquebox/metadata/EnvironmentMetaData.java
@@ -0,0 +1,27 @@
+package org.torquebox.metadata;
+
+public class EnvironmentMetaData {
+	
+	private String environmentName = "development";
+	private boolean developmentMode = true;
+	
+	public EnvironmentMetaData() {
+	}
+	
+	public void setDevelopmentMode(boolean developmentMode) {
+		this.developmentMode = developmentMode;
+	}
+	
+	public boolean isDevelopmentMode() {
+		return this.developmentMode;
+	}
+	
+	public void setEnvironmentName(String environmentName) {
+		this.environmentName = environmentName;
+	}
+	
+	public String getEnvironmentName() {
+		return this.environmentName;
+	}
+
+}
