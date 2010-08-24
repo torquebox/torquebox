a0c61fdfec7312bf6d215a11308f225212c12d94
Bob McWhirter <bob@mcwhirter.org>
2010-08-24 13:21:02 -0400

    Improve IO.read(..) to work with relative paths.

--- components/vfs/lib/vfs/ext/io.rb
+++ components/vfs/lib/vfs/ext/io.rb
@@ -101,15 +101,13 @@ class IO
     def read(name, length=nil, offset=nil)
       return read_without_vfs(name, length, offset) if ::File.exist_without_vfs?( name )
 
-      #puts "IO#read(#{name})"
-      vfs_url, child_path = VFS.resolve_within_archive(name)
-      #puts "vfs_url=#{vfs_url}"
-      #puts "child_path=#{child_path}"
-      raise ::Errno::ENOENT unless vfs_url
-
-      virtual_file = Java::org.jboss.vfs.VFS.child( vfs_url )
-      virtual_file = virtual_file.getChild( child_path ) if child_path
-      raise ::Errno::ENOENT unless virtual_file
+      if ( name =~ /^\// || name =~ /^vfs:\// ) 
+        full_path = name
+      else
+        full_path = File.join( Dir.pwd, name )
+      end
+      virtual_file = org.jboss.vfs.VFS.child( full_path )
+      raise ::Errno::ENOENT.new( "#{name} (#{virtual_file})" ) unless virtual_file.exists()
 
       stream = virtual_file.openStream()
       io = stream.to_io 
