c8df375ad847495310760770a5c5f8c1d0338e9e
jcrossley <jim@crossleys.org>
2010-09-17 13:36:52 -0400

    Fixes TORQUE-122

--- components/rake-support/share/rails-template.rb
+++ components/rake-support/share/rails-template.rb
@@ -4,6 +4,16 @@ gem "activerecord-jdbc-adapter",
 
 rakefile( 'torquebox.rake' ) do
   <<-TASK
-    require 'torquebox/tasks'
+require 'torquebox/tasks'
+
+# Patch db:load_config to make sure activerecord-jdbc-adapter gets loaded
+namespace :db do
+  task :load_config => :rails_env do
+    require 'active_record'
+    require 'activerecord-jdbc-adapter'
+    ActiveRecord::Base.configurations = Rails::Configuration.new.database_configuration
+  end
+end
+
   TASK
 end
