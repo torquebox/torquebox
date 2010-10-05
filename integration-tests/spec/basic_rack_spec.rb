750b0cb3615e4704e2b6b38b6e950bf8e279c5c0
jcrossley <jim@crossleys.org>
2010-10-05 12:13:11 -0400

    Taking advantage of Spec::Runner.configure global blocks.
    The test need only provide a deployment_name.

--- integration-tests/spec/basic_rack_spec.rb
+++ integration-tests/spec/basic_rack_spec.rb
@@ -2,12 +2,9 @@ require 'container'
 require 'open-uri'
 
 describe "basic rack test" do
-  include TorqueBox::TestContainer
-  before(:all) do
-    start "rack/1.1.0/basic-rack.yml"
-  end
-  after(:all) do
-    stop
+
+  def deployment_name
+    "rack/1.1.0/basic-rack.yml"
   end
 
   it "should not get a 500 server error" do
