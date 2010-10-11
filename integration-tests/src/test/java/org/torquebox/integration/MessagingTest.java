be303076a7221955484cf4835c0988a4fa9d827a
Bob McWhirter <bob@mcwhirter.org>
2010-10-11 15:42:35 -0400

    add some more debug to the test.  receive() is calling, but returning nil.

--- integration-tests/src/test/java/org/torquebox/integration/MessagingTest.java
+++ integration-tests/src/test/java/org/torquebox/integration/MessagingTest.java
@@ -2,28 +2,17 @@ package org.torquebox.integration;
 
 import static org.junit.Assert.assertEquals;
 
-import java.rmi.RMISecurityManager;
-
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.api.Run;
 import org.jboss.arquillian.api.RunModeType;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jruby.Ruby;
-import org.jruby.RubyString;
-import org.junit.Before;
 import org.junit.Test;
 
 
 @Run(RunModeType.AS_CLIENT)
 public class MessagingTest extends AbstractIntegrationTest {
     
-    @Before
-    public void setUpSecurityManager() {
-        if(System.getSecurityManager() == null) {
-            System.setSecurityManager(new RMISecurityManager());
-        }
-    }
-
 	@Deployment
 	public static JavaArchive createDeployment() {
 		return createDeployment( "rails/2.3.8/messaging-rails.yml" );
@@ -32,9 +21,10 @@ public class MessagingTest extends AbstractIntegrationTest {
 	@Test
 	public void testQueueMessageProcessor() throws Exception {
         driver.get( "http://localhost:8080/messaging-rails/message/queue?text=ham%20biscuit" );
-        Ruby runtime = IntegrationTestRubyFactory.createRuby();
         
-        RubyString result = (RubyString) runtime.evalScriptlet( slurpResource( "org/torquebox/integration/messaging_test.rb" ) );
+        Ruby runtime = IntegrationTestRubyFactory.createRuby();
+        Object result = runtime.evalScriptlet( slurpResource( "org/torquebox/integration/messaging_test.rb" ) );
+        System.err.println(" result=" + result );
         
         assertEquals( "result=ham biscuit", result.toString() );
 	}
--- integration-tests/src/test/java/org/torquebox/integration/messaging_test.rb
+++ integration-tests/src/test/java/org/torquebox/integration/messaging_test.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'org.torquebox.torquebox-messaging-client'
 
-#TorqueBox::Naming.configure do |config|
-  #config.host = "127.0.0.1"
-  #config.port = 1099
-#end
+result = TorqueBox::Messaging::Queue.new('/queues/results').receive(:timeout => 2000)
 
-TorqueBox::Messaging::Queue.new('/queues/results').receive(:naming_host=>'127.0.0.1', :naming_port=>1099, :timeout => 2000)
+puts "result from receive is #{result.inspect}"
+
+result
