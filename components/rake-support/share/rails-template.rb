d0d0d266f7e528610a67c22b6d1ae3bbad0b3965
jcrossley <jim@crossleys.org>
2010-10-20 12:34:52 -0400

    Gussy up the app template for rails 3.

--- components/rake-support/share/rails-template.rb
+++ components/rake-support/share/rails-template.rb
@@ -1,7 +1,18 @@
 
-gem "activerecord-jdbc-adapter",
-    :require=>'jdbc_adapter'
+# Comment out the MRI sqlite3 gem requiring native extensions
+run %q[sed -i '' -e 's/^\(gem.*sqlite3\)/# \1/' Gemfile]
 
+# Create app/tasks and app/jobs, just for fun
+inside('app') {
+  FileUtils.mkdir %w( tasks jobs )
+}
+
+# We need the jdbc adapter
+gem "activerecord-jdbc-adapter", :require=>'arjdbc'
+# We need the torquebox rake tasks 
+gem "org.torquebox.rake-support"
+
+# We need the app to find the rake tasks
 rakefile( 'torquebox.rake' ) do
   <<-TASK
 require 'torquebox/tasks'
