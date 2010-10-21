7f216d71fc46858c3dc6b54891a7015dd2c59acb
jcrossley <jim@crossleys.org>
2010-10-21 19:04:39 -0400

    Seems to me this should work for manually-assembled rack archives containing both a config.ru and a web.yml.
    But it doesn't yet.
    
    Refs TORQUE-142

--- components/rack/rack-int/src/main/java/org/torquebox/rack/deployers/RackStructure.java
+++ components/rack/rack-int/src/main/java/org/torquebox/rack/deployers/RackStructure.java
@@ -6,28 +6,52 @@ import org.jboss.deployers.spi.DeploymentException;
 import org.jboss.deployers.spi.structure.ContextInfo;
 import org.jboss.deployers.vfs.spi.structure.StructureContext;
 import org.torquebox.mc.vdf.AbstractRubyStructureDeployer;
+import org.jboss.vfs.VirtualFile;
+import org.jboss.deployers.spi.attachments.MutableAttachments;
+import org.torquebox.rack.metadata.RackApplicationMetaData;
 
 public class RackStructure extends AbstractRubyStructureDeployer {
 
-	@Override
-	protected boolean doDetermineStructure(StructureContext structureContext) throws DeploymentException {
+    public RackStructure() {
+        // We should come after RailsStructure, since Rails 3 apps
+        // include a config.ru, which we would mistake for pure rack
+        setRelativeOrder( -999 );
+    }
 
-		ContextInfo context = createContext(structureContext, new String[] { "config" });
+    @Override
+    protected boolean doDetermineStructure(StructureContext structureContext) throws DeploymentException {
+        VirtualFile file = structureContext.getFile();
+        ContextInfo context = null;
+        try {
+            VirtualFile rackup = file.getChild("config.ru");
+            if (rackup != null && rackup.exists()) {
+                context = createContext(structureContext, new String[] { "config" });
+                addDirectoryOfJarsToClasspath(structureContext, context, "lib/java");
+                addRackApplicationMetaData( structureContext, context );
+                return true;
+            }
+        } catch (IOException e) {
+            if (context != null) structureContext.removeChild(context);
+            throw new DeploymentException(e);
+        }
+        return false;
+    }
 
-		try {
-			addDirectoryOfJarsToClasspath(structureContext, context, "lib/java");
-		} catch (IOException e) {
-			structureContext.removeChild(context);
-			throw new DeploymentException(e);
-		}
-		
-		return true;
+    protected void addRackApplicationMetaData(StructureContext structureContext, ContextInfo context) throws IOException {
+        MutableAttachments attachments = (MutableAttachments) context.getPredeterminedManagedObjects();
+        RackApplicationMetaData rackAppMetaData = new RackApplicationMetaData();
+        rackAppMetaData.setRackRoot( structureContext.getRoot() );
+        attachments.addAttachment( RackApplicationMetaData.class, rackAppMetaData );
+    }
 
-	}
+    @Override
+    protected boolean hasValidSuffix(String name) {
+        return name.endsWith(".rack");
+    }
 
-	@Override
-	protected boolean hasValidSuffix(String name) {
-		return name.endsWith(".rack");
-	}
+    @Override
+    protected boolean hasValidName(VirtualFile file) {
+        return hasValidSuffix( file.getName() );
+    }
 
 }
