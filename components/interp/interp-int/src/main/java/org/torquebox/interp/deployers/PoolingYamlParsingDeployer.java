f0cc093681c0b343a17007ef0ebeaccc494c875b
jcrossley <jim@crossleys.org>
2010-11-02 20:37:46 -0400

    Added a couple of tests for TORQUE-148.
    One of them is commented until we figure out why one deployer gets one
    unit and the other deployer gets another.

--- components/interp/interp-int/src/main/java/org/torquebox/interp/deployers/PoolingYamlParsingDeployer.java
+++ components/interp/interp-int/src/main/java/org/torquebox/interp/deployers/PoolingYamlParsingDeployer.java
@@ -8,6 +8,7 @@ import org.jboss.deployers.vfs.spi.deployer.AbstractVFSParsingDeployer;
 import org.jboss.deployers.vfs.spi.structure.VFSDeploymentUnit;
 import org.jboss.vfs.VirtualFile;
 import org.torquebox.interp.metadata.PoolMetaData;
+import org.torquebox.mc.AttachmentUtils;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.error.YAMLException;
 
@@ -87,7 +88,7 @@ public class PoolingYamlParsingDeployer extends AbstractVFSParsingDeployer<PoolM
                         }
                     }
                     log.info("Configured Ruby runtime pool: "+poolMetaData);
-                    unit.addAttachment(PoolMetaData.class.getName() + "$" + name, poolMetaData, PoolMetaData.class);
+                    AttachmentUtils.multipleAttach( unit, poolMetaData, name );
                 }
             }
         } catch (YAMLException e) {
