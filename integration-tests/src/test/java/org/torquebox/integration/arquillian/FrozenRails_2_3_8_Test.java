7e7e16c15479ec882d69ac0bcbef6f45cf9f9e28
Bob McWhirter <bob@mcwhirter.org>
2010-11-10 16:24:34 -0500

    Test the frozen app.

---
+++ integration-tests/src/test/java/org/torquebox/integration/arquillian/FrozenRails_2_3_8_Test.java
@@ -0,0 +1,30 @@
+package org.torquebox.integration.arquillian;
+
+import static org.junit.Assert.*;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.api.Run;
+import org.jboss.arquillian.api.RunModeType;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+@Run(RunModeType.AS_CLIENT)
+public class FrozenRails_2_3_8_Test extends AbstractIntegrationTest {
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return createDeployment( "rails/2.3.8/frozen-rails.yml" );
+	}
+
+	@Test
+	public void testHighLevel() {
+        driver.get( "http://localhost:8080/frozen-rails" );
+        System.err.println( "RESULT: " );
+        System.err.println( driver.getPageSource() );
+        WebElement element = driver.findElementById( "success" );
+        assertNotNull( element );
+        assertEquals( "basic-rails", element.getAttribute( "class" ) );
+	}
+
+}
