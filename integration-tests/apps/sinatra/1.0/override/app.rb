cd98837567668aacb24fd5b4b3218d14c70928bf
jcrossley <jim@crossleys.org>
2010-11-18 16:18:16 -0500

    Minor

--- integration-tests/apps/sinatra/1.0/override/app.rb
+++ integration-tests/apps/sinatra/1.0/override/app.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 
-get '/' do
+get '/:k' do
+  ENV[params[:k]] || all
+end
+
+def all
   "<pre>\n" + ENV.keys.sort.map { |k| "#{k}=#{ENV[k]}" }.join("\n") + "</pre>"
 end
-
-get '/:k' do
-  ENV[params[:k]]
-end
-
