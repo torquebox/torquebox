035d379ee1e73682bf5de31b323db6cac21194a7
jcrossley <jim@crossleys.org>
2010-11-19 15:06:16 -0500

    Self-awareness

--- components/messaging/messaging-core/src/main/java/org/torquebox/messaging/core/ManagedQueue.java
+++ components/messaging/messaging-core/src/main/java/org/torquebox/messaging/core/ManagedQueue.java
@@ -1,22 +1,19 @@
 package org.torquebox.messaging.core;
 
-
 public class ManagedQueue extends AbstractManagedDestination {
-	
-	public ManagedQueue() {
-		
-	}
-	
-	public void start() throws Exception {
-		// System.err.println( "start queue " + getName() );
-		
-		getServer().createQueue(false, getName(), "", false, getName() );
-	}
-	
-	public void destroy() throws Exception {
-		// System.err.println( "destroy queue " + getName() );
-		
-		getServer().destroyQueue( getName() );
-	}
+    
+    public ManagedQueue() {
+        
+    }
+    
+    public void start() throws Exception {
+        log.info("Starting queue: "+getName());
+        getServer().createQueue(false, getName(), "", false, getName() );
+    }
+    
+    public void destroy() throws Exception {
+        log.info("Destroying queue: "+getName());
+        getServer().destroyQueue( getName() );
+    }
 
 }
