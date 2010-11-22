8bfdd52296902e5185d0cd3b42dd10ddb6c4d31a
jcrossley <jim@crossleys.org>
2010-11-22 18:38:36 -0500

    Higher fidelity

--- integration-tests/apps/sinatra/1.0/queues/upper_caser.rb
+++ integration-tests/apps/sinatra/1.0/queues/upper_caser.rb
@@ -2,6 +2,7 @@ require 'org.torquebox.torquebox-messaging-client'
 
 class UpperCaser < TorqueBox::Messaging::MessageProcessor
   def initialize
+    puts "JC: UpperCaser initializing..."
     @queue = TorqueBox::Messaging::Queue.new '/queues/responses'
     puts "JC: UpperCaser initialized: #@queue"
   end
