adf98754bbe2f98300b861118ddb7b1d1b94bfa3
Bob McWhirter <bob@mcwhirter.org>
2010-12-04 14:15:10 -0500

    Show how to use Servlet-based sessions in Rails3 applications.

--- integration-tests/apps/rails/3.0.0/basic/config/initializers/session_store.rb
+++ integration-tests/apps/rails/3.0.0/basic/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Basic::Application.config.session_store TorqueBox::Session::ServletStore
+Basic::Application.config.session_store TorqueBox::Session::ServletStore if defined?(TorqueBox::Session::ServletStore)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
