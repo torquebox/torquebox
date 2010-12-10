b631b46a916b105e24972dca630b9b5031b3f9b7
jcrossley <jim@crossleys.org>
2010-12-10 14:48:42 -0500

    Fixes TORQUE-181

--- components/rack/rack-core/src/test/java/org/torquebox/rack/core/RackEnvironmentImplTest.java
+++ components/rack/rack-core/src/test/java/org/torquebox/rack/core/RackEnvironmentImplTest.java
@@ -68,7 +68,6 @@ public class RackEnvironmentImplTest extends AbstractRubyTestCase {
 		//Map<String, Object> javaEnv = (Map<String, Object>) rubyEnv.toJava(Map.class);
 		RackEnvironment env = new RackEnvironmentImpl( ruby, servletContext, servletRequest );
 		RubyHash envHash = env.getEnv();
-		System.err.println( envHash.get( "REQUEST_METHOD" ) );
 		assertNotNull(envHash);
 
 		assertEquals("GET", envHash.get("REQUEST_METHOD"));
@@ -127,7 +126,6 @@ public class RackEnvironmentImplTest extends AbstractRubyTestCase {
 		//Map<String, Object> javaEnv = (Map<String, Object>) rubyEnv.toJava(Map.class);
 		RackEnvironment env = new RackEnvironmentImpl( ruby, servletContext, servletRequest );
 		RubyHash envHash = env.getEnv();
-		System.err.println( envHash.get( "REQUEST_METHOD" ) );
 		assertNotNull(envHash);
 
 		assertEquals("GET", envHash.get("REQUEST_METHOD"));
@@ -148,6 +146,22 @@ public class RackEnvironmentImplTest extends AbstractRubyTestCase {
 		assertNotNull(envHash.get("rack.errors"));
 		assertSame(servletRequest, envHash.get("servlet_request"));
 		assertSame(servletRequest, envHash.get("java.servlet_request"));
+	}
+	
+	@Test
+	public void testMissingContentLength() throws Exception {
+
+		final ServletContext servletContext = mock(ServletContext.class);
+		final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+		final ServletInputStream inputStream = new MockServletInputStream(new ByteArrayInputStream("".getBytes()));
+
+		when(servletRequest.getInputStream()).thenReturn(inputStream);
+		when(servletRequest.getContentLength()).thenReturn(-1);
+
+		RackEnvironment env = new RackEnvironmentImpl( ruby, servletContext, servletRequest );
+		RubyHash envHash = env.getEnv();
+		assertNotNull(envHash);
+		assertNull(envHash.get("CONTENT_LENGTH"));
 	}
 	
 	@SuppressWarnings("unchecked")
