11c804c7820e9255c7f6a16dae5ece812e532c5a
jcrossley <jim@crossleys.org>
2010-12-13 16:30:56 -0500

    Deuglification of Bob's working test.

--- integration-tests/spec/basic_rack_spec.rb
+++ integration-tests/spec/basic_rack_spec.rb
@@ -3,28 +3,7 @@ require 'open-uri'
 
 describe "basic rack test" do
 
-  class << self 
-    def create_deployment()
-      deployment = "rack/1.1.0/basic-rack.yml"
-      puts "BOB: DEPLOYING deployment = #{deployment}"
-      tail = deployment.split('/')[-1]
-      puts "BOB: DEPLOYING tail = #{tail}"
-      base = /(.*)\./.match(tail)[1]
-      puts "BOB: DEPLOYING base = #{base}"
-      archive = org.jboss.shrinkwrap.api.ShrinkWrap.create( org.jboss.shrinkwrap.api.spec.JavaArchive.java_class, "#{base}.jar" )
-      puts "BOB: DEPLOYING archive = #{archive}"
-      deploymentDescriptorUrl = JRuby.runtime.jruby_class_loader.getResource( deployment )
-      puts "BOB: DEPLOYING url = #{deploymentDescriptorUrl}"
-      archive.addResource( deploymentDescriptorUrl, "/META-INF/#{tail}" )
-      puts "Deploying #{archive}"
-      archive
-    end
-
-    add_method_signature("create_deployment", [org.jboss.shrinkwrap.api.spec.JavaArchive.to_java])
-    add_method_annotation( "create_deployment", org.jboss.arquillian.api.Deployment => {} )
-  end
-
-  add_class_annotation( org.jboss.arquillian.api.Run => { "value" => org.jboss.arquillian.api.RunModeType::AS_CLIENT } )
+  deploy "rack/1.1.0/basic-rack.yml", :run_mode => :client
 
   it "should not get a 500 server error" do
     result = open("http://localhost:8080/basic-rack").read
--- integration-tests/spec/container.rb
+++ integration-tests/spec/container.rb
@@ -4,14 +4,24 @@ module Spec
   module Example
     module ExampleGroupMethods
 
-=begin
-      def deploy( path, options = {} )
+      def deploy path, options = {}
         @run_mode = options.fetch(:run_mode, :client)
-        @deployment = path
+        add_class_annotation( org.jboss.arquillian.api.Run => { "value" => run_mode } )
+        metaclass = class << self
+                      add_method_signature( "create_deployment", [org.jboss.shrinkwrap.api.spec.JavaArchive] )
+                      add_method_annotation( "create_deployment", org.jboss.arquillian.api.Deployment => {} )
+                      self
+                    end
+        metaclass.send(:define_method, :create_deployment) do
+          tail = path.split('/')[-1]
+          base = /(.*)\./.match(tail)[1]
+          archive = org.jboss.shrinkwrap.api.ShrinkWrap.create( org.jboss.shrinkwrap.api.spec.JavaArchive.java_class, "#{base}.jar" )
+          deploymentDescriptorUrl = JRuby.runtime.jruby_class_loader.getResource( path )
+          archive.addResource( deploymentDescriptorUrl, "/META-INF/#{tail}" )
+          archive
+        end
       end
 
-      attr_reader :deployment
-      
       def run_mode
         case @run_mode
         when :client
@@ -23,16 +33,6 @@ module Spec
         end 
       end
 
-      def create_deployment 
-        tail = @deployment.split('/')[-1]
-        base = /(.*)\./.match(tail)[1]
-        archive = org.jboss.shrinkwrap.api.ShrinkWrap.create( org.jboss.shrinkwrap.api.spec.JavaArchive.java_class, "#{base}.jar" )
-        deploymentDescriptorUrl = JRuby.runtime.jruby_class_loader.getResource( name )
-        archive.addResource( deploymentDescriptorUrl, "/META-INF/#{tail}" )
-        archive
-      end
-=end
-
     end
   end
 end
@@ -40,26 +40,22 @@ end
 Spec::Runner.configure do |config|
 
   config.before(:suite) do
-    puts "JC: before(:suite)"
     configuration = org.jboss.arquillian.impl.XmlConfigurationBuilder.new.build()
     Thread.current[:test_runner_adaptor] = org.jboss.arquillian.impl.DeployableTestBuilder.build(configuration)
     Thread.current[:test_runner_adaptor].beforeSuite
   end
     
   config.before(:all) do
-    puts "JC: before(:all)"
-    @real_java_class = self.class.become_java!('.')
+    @real_java_class = self.class.become_java!  # ('.') 
     Thread.current[:test_runner_adaptor].beforeClass(@real_java_class)
   end
 
   config.after(:all) do
-    puts "JC: after(:all)"
     Thread.current[:test_runner_adaptor].afterClass(@real_java_class)
   end
 
   config.after(:suite) do
-    puts "JC: after(:suite)"
-    Thread.current[:test_runner_adaptor].afterSuite 
+    Thread.current[:test_runner_adaptor].afterSuite rescue nil
   end
 
 end
