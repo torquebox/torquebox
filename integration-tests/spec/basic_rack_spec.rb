03238ac9e033e38467524c30d217fb143a802d28
jcrossley <jim@crossleys.org>
2010-12-14 21:44:45 -0500

    Actually using capybara now.

--- integration-tests/spec/basic_rack_spec.rb
+++ integration-tests/spec/basic_rack_spec.rb
@@ -1,21 +1,12 @@
-require 'container'
-require 'open-uri'
-
-require 'rubygems'
-require 'capybara'
+require 'spec_helper'
 
 describe "basic rack test" do
 
   deploy :path => "rack/1.1.0/basic-rack.yml"
 
-  # Or you can pass a block...
-  # deploy do 
-  #   create_archive_for_resource( "rack/1.1.0/basic-rack.yml" )
-  # end
-
-  it "should not get a 500 server error" do
-    result = open("http://localhost:8080/basic-rack").read
-    puts "CAPYBARA! #{Capybara.inspect}"
+  it "should work" do
+    visit "/basic-rack"
+    page.should have_content('it worked')
   end
 
 end
