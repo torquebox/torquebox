2fac5bcb8fe4965f0517abe038062e574c94ed04
Bob McWhirter <bob@mcwhirter.org>
2010-12-17 15:26:06 -0500

    Have torquebox:freeze do bundler package and install with a local path if a Gemfile is present.  torquebox:archive invokes.

--- components/rake-support/lib/torquebox/tasks/archive.rb
+++ components/rake-support/lib/torquebox/tasks/archive.rb
@@ -1,4 +1,6 @@
 
+require 'rbconfig'
+
 def get_archive_name root=Dir.pwd
   File.basename(root) + (rails?(root) ? '.rails' : '.rack')
 end
@@ -6,8 +8,8 @@ end
 namespace :torquebox do
 
   desc "Create a nice self-contained application archive"
-  task :archive do 
-    skip_files = %w{ ^log$ ^tmp$ \.rails$ \.rack$ }
+  task :archive=>[:freeze] do 
+    skip_files = %w{ ^log$ ^tmp$ \.rails$ \.rack$ vendor }
 
     include_files = []
     Dir[ "*", ".bundle" ].each do |entry|
@@ -15,6 +17,10 @@ namespace :torquebox do
       unless ( skip_files.any?{ |regex| entry.match(regex)} )
         include_files << entry
       end
+    end
+
+    Dir[ 'vendor/*' ].each do |entry|
+      include_files << entry unless ( entry == 'vendor/cache' )
     end
 
     archive_name = get_archive_name
@@ -42,15 +48,36 @@ namespace :torquebox do
   if ( File.exist?( 'Gemfile' ) )
     desc "Freeze application gems"
     task :freeze do
-      require 'bundler/cli'
-
-      cli = Bundler::CLI.new( :deployment=>true, :path=>'vendor/bundle' )
-      cli.config
-      cli.install
-
+      jruby = File.join( RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'] )
+      exec_cmd( "#{jruby} -S bundle package" )
+      exec_cmd( "#{jruby} -S bundle install --local --path vendor/bundle" )
+    end
+  else
+    task :freeze do
+      # nothing
     end
   end
 
 end
 
 
+def exec_cmd(cmd)
+  Open3.popen3( cmd ) do |stdin, stdout, stderr|
+    stdin.close
+    stdout_thr = Thread.new(stdout) {|stdout_io|
+      stdout_io.each_line do |l|
+        STDOUT.puts l
+        STDOUT.flush
+      end
+      stdout_io.close
+    }
+    stderr_thr = Thread.new(stderr) {|stderr_io|
+      stderr_io.each_line do |l|
+        STDERR.puts l
+        STDERR.flush
+      end
+    }
+    stdout_thr.join
+    stderr_thr.join
+  end
+end
