059ff4ce8a264d4b43b61497d631bf95da2b9586
jcrossley <jim@crossleys.org>
2010-12-16 23:35:06 -0500

    Removing a dumb, failing test.

--- integration-tests/apps/rails/2.x/messaging/app/controllers/message_controller.rb
+++ integration-tests/apps/rails/2.x/messaging/app/controllers/message_controller.rb
@@ -1,4 +1,3 @@
-require '/Users/jim/local/torquebox/jruby/lib/ruby/gems/1.8/gems/org.torquebox.torquebox-container-foundation-1.0.0.RC1-java/lib/jboss-reloaded-vdf-bootstrap-minimal-0.1.2.jar'
 
 class MessageController < ApplicationController
 
@@ -10,28 +9,6 @@ class MessageController < ApplicationController
     msg = params[:text] || "nothing"
     TorqueBox::Messaging::Queue.new('/queues/test').publish(msg)
     redirect_to root_path
-  end
-
-
-  # Test programmitic creation/destruction of queues
-
-  def start
-    @@queue = TorqueBox::Messaging::Queue.new "/queues/#{params[:name]}"
-    @@queue.start
-    puts "JC: about to create dispatcher"
-    @@dispatcher = TorqueBox::Messaging::Dispatcher.new do
-      map TestConsumer, @@queue
-      puts "JC: consumers mapped"
-    end
-    @@dispatcher.start
-    puts "JC: dispatcher started"
-  rescue Exception 
-    puts "JC: wtf? #{$!} #{$@}"
-  end
-
-  def stop
-    @@dispatcher.stop
-    @@queue.destroy
   end
 
 end
--- integration-tests/spec/messaging_spec.rb
+++ integration-tests/spec/messaging_spec.rb
@@ -2,23 +2,13 @@ require 'spec_helper'
 require 'org.torquebox.torquebox-messaging-client'
 
 describe "messaging rack test" do
+
   deploy :path => "rails/2.x/messaging-rails.yml"
 
   it "should receive a ham biscuit" do
     visit "/messaging-rails/message/queue?text=ham+biscuit"
     result = TorqueBox::Messaging::Queue.new('/queues/results').receive(:timeout => 2000)
     result.should == "result=ham biscuit"
-  end
-
-  it "should create queues and consumers dynamically" do
-    visit "/messaging-rails/message/start?name=lanceball"
-    puts "JC: visited start"
-    TorqueBox::Messaging::Queue.new("/queues/lanceball").publish("nouns and verbs")
-    puts "JC: published"
-    result = TorqueBox::Messaging::Queue.new("/queues/results").receive(:timeout => 2000)
-    puts "JC: received"
-    result.should == "result=nouns and verbs"
-    visit "/messaging-rails/message/stop"
   end
 
 end
