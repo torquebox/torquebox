51cb3ad9d504838db390a0d78b390b04deeb90f7
jcrossley <jim@crossleys.org>
2010-12-23 16:29:34 -0500

    A new RubyServiceProxy class, capable of starting a long-running ruby service. [TORQUE-192]

---
+++ components/jobs/jobs-core/src/main/java/org/torquebox/jobs/core/RubyServiceProxy.java
@@ -0,0 +1,46 @@
+package org.torquebox.jobs.core;
+
+import org.jruby.Ruby;
+import org.jruby.javasupport.JavaEmbedUtils;
+import org.jruby.runtime.builtin.IRubyObject;
+
+import org.torquebox.common.reflect.ReflectionHelper;
+import org.torquebox.interp.spi.RubyRuntimePool;
+import org.torquebox.interp.core.RubyComponentResolver;
+
+
+public class RubyServiceProxy {
+
+	public void setRubyRuntimePool(RubyRuntimePool runtimePool) {
+		this.runtimePool = runtimePool;
+	}
+	
+    public void setRubyComponentResolver(RubyComponentResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    public void start() throws Exception {
+        if (this.ruby != null) throw new IllegalStateException("Already running");
+        this.ruby = runtimePool.borrowRuntime();
+        ReflectionHelper.callIfPossible(this.ruby, getService(), "start", null);
+    }
+
+    public void stop() throws Exception {
+        if (this.ruby == null) throw new IllegalStateException("Not running");
+        ReflectionHelper.callIfPossible(this.ruby, getService(), "stop", null);
+        runtimePool.returnRuntime(this.ruby);
+        this.ruby = null;
+    }
+
+    protected IRubyObject getService() throws Exception {
+        if (this.service == null) {
+            this.service = resolver.resolve(this.ruby);
+        }
+        return this.service;
+    }
+
+	private RubyRuntimePool runtimePool;
+    private Ruby ruby;
+    private IRubyObject service;
+    private RubyComponentResolver resolver;
+}
---
+++ components/jobs/jobs-core/src/test/java/org/torquebox/jobs/core/RubyServiceProxyTest.java
@@ -0,0 +1,61 @@
+package org.torquebox.jobs.core;
+
+import org.junit.*;
+import java.net.*;
+import java.util.*;
+import static org.junit.Assert.*;
+
+import org.jruby.Ruby;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.javasupport.JavaEmbedUtils;
+
+import org.torquebox.interp.core.SharedRubyRuntimePool;
+import org.torquebox.interp.core.InstantiatingRubyComponentResolver;
+import org.torquebox.test.ruby.AbstractRubyTestCase;
+import org.torquebox.common.reflect.ReflectionHelper;
+
+public class RubyServiceProxyTest extends AbstractRubyTestCase {
+
+	private Ruby ruby;
+    private SharedRubyRuntimePool pool;
+    private InstantiatingRubyComponentResolver resolver;
+    private RubyServiceProxy proxy;
+
+	@Before
+	public void setUp() throws Exception {
+		this.ruby = createRuby();
+        this.pool = new SharedRubyRuntimePool(this.ruby);
+        this.proxy = new RubyServiceProxy();
+        this.proxy.setRubyRuntimePool(this.pool);
+	}
+
+    @Test public void testServiceStartStop() throws Exception {
+        resolver = new InstantiatingRubyComponentResolver();
+        proxy.setRubyComponentResolver( resolver );
+
+        resolver.setRubyClassName( "TestService" );
+        resolver.setRubyRequirePath( "org/torquebox/jobs/core/test_service" );
+        
+        proxy.start();
+        Boolean started = (Boolean) ReflectionHelper.callIfPossible(this.ruby, proxy.getService(), "started?", null);
+        assertTrue( started.booleanValue() );
+
+        proxy.stop();
+        started = (Boolean) ReflectionHelper.callIfPossible(this.ruby, proxy.getService(), "started?", null);
+        assertFalse( started.booleanValue() );
+    }
+
+    @Test public void testInitialization() throws Exception {
+        resolver = new InstantiatingRubyComponentResolver();
+        proxy.setRubyComponentResolver( resolver );
+
+        resolver.setRubyClassName( "TestService" );
+        resolver.setRubyRequirePath( "org/torquebox/jobs/core/test_service" );
+        resolver.setInitializeParams( ruby.evalScriptlet("{'foo'=>42}").convertToHash() );
+        
+        proxy.start();
+        assertEquals( 42, JavaEmbedUtils.invokeMethod(ruby, proxy.getService(), "[]", new Object[] { "foo" }, Integer.class) );
+        proxy.stop();
+    }
+
+}
