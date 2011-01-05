9946ab461fb0f49363af60f68e09c88ab98ee0f5
Bob McWhirter <bob@mcwhirter.org>
2011-01-05 11:21:42 -0500

    Push additional specs to using test-data helper.

--- components/vfs/spec/vfs_dir_spec.rb
+++ components/vfs/spec/vfs_dir_spec.rb
@@ -3,18 +3,7 @@ require File.dirname(__FILE__) +  '/spec_helper.rb'
 
 describe "VFS::Dir" do
 
-  before(:each) do
-    @executor = java.util.concurrent::Executors.newScheduledThreadPool( 1 )
-    @temp_file_provider = org.jboss.vfs::TempFileProvider.create( "vfs-test", @executor )
-    @archive1_path = File.expand_path( "#{TEST_DATA_DIR}/home/larry/archive1.jar" )
-    @archive1_file = org.jboss.vfs::VFS.child( @archive1_path )
-    @archive1_mount_point = org.jboss.vfs::VFS.child( @archive1_path )
-    @archive1_handle = org.jboss.vfs::VFS.mountZip( @archive1_file, @archive1_mount_point, @temp_file_provider )
-  end
-
-  after(:each) do
-    @archive1_handle.close
-  end
+  extend TestDataHelper
 
   describe "entries" do
     it "should find vfs entries outside of archives" do
