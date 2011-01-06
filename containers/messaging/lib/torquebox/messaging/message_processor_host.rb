e7e363e84d0cbadf20866b7e4182879ca9c370c1
Bob McWhirter <bob@mcwhirter.org>
2011-01-06 13:00:30 -0500

    Adjust HornetQ configuration to ensure THREE slashes after file: in file:///C:/urls.

--- containers/messaging/lib/torquebox/messaging/message_processor_host.rb
+++ containers/messaging/lib/torquebox/messaging/message_processor_host.rb
@@ -15,8 +15,14 @@ module TorqueBox
 =begin
       def before_start(container)
         org.hornetq.core.logging::Logger.setDelegateFactory( org.hornetq.integration.logging::Log4jLogDelegateFactory.new )
-        Java::java.lang::System.setProperty( "torquebox.hornetq.configuration.url", 
-                                             "file://" + File.join( File.dirname(__FILE__), 'hornetq-configuration.xml' ) )
+
+	config_path = File.expand_path( File.join( File.dirname(__FILE__), 'hornetq-configuration.xml' )  )
+	if ( config_path[0,1] != '/' )
+	  config_path = "/#{config_path}"
+	end
+	config_url = "file://#{config_path}"
+        java.lang::System.setProperty( "torquebox.hornetq.configuration.url", 
+                                       config_url )
       end
 =end
     
