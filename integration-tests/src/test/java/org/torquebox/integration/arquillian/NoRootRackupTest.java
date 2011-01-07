ba1a112dc6cb42d06634434d3f4befa7e8ca0104
Bob McWhirter <bob@mcwhirter.org>
2011-01-07 10:52:31 -0500

    Split the Rack Reloader test into Unix-vs-Windows, since Windows filesystem sync/updates enjoy a leisurely mosey.  Windows test does at least verify Reloading, but is a weaker test with regards to timing.

--- integration-tests/src/test/java/org/torquebox/integration/arquillian/NoRootRackupTest.java
+++ integration-tests/src/test/java/org/torquebox/integration/arquillian/NoRootRackupTest.java
@@ -21,14 +21,9 @@ public class NoRootRackupTest extends AbstractIntegrationTest {
 	public void testHappiness() {
         driver.get( "http://localhost:8080/norootrackup" );
         WebElement body = driver.findElementByTagName("body");
-        
         String expectedRoot = System.getProperty("basedir");
-        
-        System.err.println( "BOB: expectedRoot.1=" + expectedRoot );
         expectedRoot = expectedRoot.replaceAll( "\\\\", "/" );
-        System.err.println( "BOB: expectedRoot.2=" + expectedRoot );
         expectedRoot = toVfsPath( expectedRoot + "/apps/rack/1.1.0/norootrackup" );
-        System.err.println( "BOB: expectedRoot.3=" + expectedRoot );
         
         assertEquals("RACK_ROOT=" + expectedRoot, body.getText().trim());
 	}
--- integration-tests/src/test/java/org/torquebox/integration/arquillian/ReloaderRackTest.java
+++ integration-tests/src/test/java/org/torquebox/integration/arquillian/ReloaderRackTest.java
@@ -1,6 +1,13 @@
 package org.torquebox.integration.arquillian;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.AssertTrue;
 
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.api.Run;
@@ -19,6 +26,15 @@ public class ReloaderRackTest extends AbstractIntegrationTest {
 
 	@Test
 	public void testReloading() throws InterruptedException {
+		if ( isWindows() ) {
+			testReloadingOnWindows();
+		} else {
+			testReloadingOnUnix();
+		}
+	}
+	
+	// DO NOT MARK AS @Test
+	public void testReloadingOnUnix() throws InterruptedException {
         WebElement element = null;
         
         driver.get( "http://localhost:8080/reloader-rack" );
@@ -32,6 +48,7 @@ public class ReloaderRackTest extends AbstractIntegrationTest {
         element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "0", element.getText() );
+        System.err.println( "==>" + element.getText() );
         
         Thread.sleep( 3000 );
         
@@ -39,16 +56,19 @@ public class ReloaderRackTest extends AbstractIntegrationTest {
         element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "1", element.getText() );
+        System.err.println( "==>" + element.getText() );
         
         driver.get( "http://localhost:8080/reloader-rack" );
         element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "1", element.getText() );
+        System.err.println( "==>" + element.getText() );
         
         driver.get( "http://localhost:8080/reloader-rack" );
         element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "1", element.getText() );
+        System.err.println( "==>" + element.getText() );
         
         Thread.sleep( 3000 );
         
@@ -56,6 +76,31 @@ public class ReloaderRackTest extends AbstractIntegrationTest {
         element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "4", element.getText() );
+        System.err.println( "==>" + element.getText() );
+	}
+	
+	// DO NOT MARK AS @Test
+	public void testReloadingOnWindows() throws InterruptedException {
+        WebElement element = null;
+        
+        Set<String> seenValues = new HashSet<String>();
+        
+        driver.get( "http://localhost:8080/reloader-rack" );
+        element = driver.findElementById( "success" );
+        assertNotNull( element );
+        assertEquals( "INITIAL", element.getText() );
+        
+        seenValues.add( element.getText() );
+        
+        for ( int i = 0 ; i < 10 ; ++i ) {
+        	Thread.sleep( 3000 );
+        	driver.get( "http://localhost:8080/reloader-rack" );
+        	element = driver.findElementById( "success" );
+        	assertNotNull( element );
+        	seenValues.add( element.getText() );
+        }
+        
+        assertTrue( seenValues.size() >= 3 );
 	}
 	
 }
