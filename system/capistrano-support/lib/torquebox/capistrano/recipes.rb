826eac80528a03f02befa9182a82f7fd57c04f51
Bob McWhirter <bob@mcwhirter.org>
2011-01-14 13:39:33 -0500

    Ya know, capistrano can deploy plain Rack applications also.

--- system/capistrano-support/lib/torquebox/capistrano/recipes.rb
+++ system/capistrano-support/lib/torquebox/capistrano/recipes.rb
@@ -1,10 +1,15 @@
 
 require 'capistrano'
 
+def is_rails?
+  return true if ( File.exists?( 'config/deploy.rb' ) )  
+  false
+end
+
 Capistrano::Configuration.instance.load do 
-  _cset( :jboss_home,         '/opt/jboss' )
-  _cset( :jboss_config,       'default'    )
-  _cset( :jboss_service_name, 'jbossas'    )
+  _cset( :jboss_home,         '/opt/jboss'   )
+  _cset( :jboss_config,       'default'      )
+  _cset( :jboss_init_script,  '/etc/init.d/jbossas' )
   
   namespace :deploy do
   
@@ -16,26 +21,26 @@ Capistrano::Configuration.instance.load do
   
     desc "Start TorqueBox Server"
     task :start do
-      puts "starting server via #{jboss_daemon_manager}"
-      run "/etc/init.d/#{jboss_service_name} start"
+      puts "Starting TorqueBox AS"
+      run "#{jboss_init_script} start"
     end
   
     desc "Stop TorqueBox Server"
     task :stop do
-      puts "stopping server via #{jboss_daemon_manager}"
-      run "/etc/init.d/#{jboss_service_name} stop"
+      puts "Stopping TorqueBox AS"
+      run "#{jboss_init_script} stop"
     end
   
     desc "Restart TorqueBox Server"
     task :restart do
-      puts "restarting server via #{jboss_daemon_manager}"
-      puts "/etc/initd/#{jboss_service_name} restart"
+      puts "Restarting TorqueBox AS"
+      puts "#{jboss_init_script} restart"
     end
   
     namespace :torquebox do
 
       task :check do
-        run "test -x /etc/init.d/#{jboss_service_name}"
+        run "test -x #{jboss_init_script}"
         run "test -d #{jboss_home}"
         run "test -d #{jboss_home}/server/#{jboss_config}"
         run "test -w #{jboss_home}/server/#{jboss_config}/deploy"
@@ -43,16 +48,20 @@ Capistrano::Configuration.instance.load do
 
       task :deployment_descriptor do
         puts "creating deployment descriptor"
+
+        root_key = is_rails? ? 'RAILS_ROOT' : 'RACK_ROOT'
     
         dd = {
           'application'=>{
-            'RAILS_ROOT'=>"#{latest_release}",
+            root_key=>"#{latest_release}",
           },
         }
     
         dd_str = YAML.dump_stream( dd )
+
+        dd_suffix = is_rails? ? 'rails' : 'rack'
     
-        dd_file = "#{jboss_home}/server/#{jboss_config}/deploy/#{application}-rails.yml"
+        dd_file = "#{jboss_home}/server/#{jboss_config}/deploy/#{application}-#{dd_suffix}.yml"
         dd_tmp_file = "#{dd_file}.tmp"
         
         cmd =  "cat /dev/null > #{dd_tmp_file}"
