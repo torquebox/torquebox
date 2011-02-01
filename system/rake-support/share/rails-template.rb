694e84d54d506a2468025bd8271edb37d2091b2c
Tobias Crawley <tcrawley@gmail.com>
2011-02-01 16:13:34 -0500

    Attempt to require torquebox rake tasks [TORQUE-242]
    This fixes an issue with rails 2.3.x not having access to the
    TorqueBox tasks if the :environment rake task is not called.

--- system/rake-support/share/rails-template.rb
+++ system/rake-support/share/rails-template.rb
@@ -56,6 +56,12 @@ inside('app') {
 rakefile( 'torquebox.rake' ) do
   <<-TASK
 
+begin
+  require 'org.torquebox.rake-support'
+rescue LoadError => ex
+  puts "Failed to load the TorqueBox rake gem (org.torquebox.rake-support). Make sure it is available in your environment."
+end
+
 # Patch db:load_config to make sure activerecord-jdbc-adapter gets loaded
 namespace :db do
   task :load_config => :rails_env do
