d7f4dfbc1946467701ac388bb59dbc1973bf1076
Ben Browning <bbrownin@redhat.com>
2011-02-02 09:15:10 -0500

    whitespace only

--- components/jobs/jobs-int/src/test/java/org/torquebox/jobs/deployers/ServicesYamlParsingDeployerTest.java
+++ components/jobs/jobs-int/src/test/java/org/torquebox/jobs/deployers/ServicesYamlParsingDeployerTest.java
@@ -12,42 +12,43 @@ import org.torquebox.test.mc.vdf.AbstractDeployerTestCase;
 import static org.junit.Assert.*;
 
 public class ServicesYamlParsingDeployerTest extends AbstractDeployerTestCase {
-	
-	private ServicesYamlParsingDeployer deployer;
 
-	@Before
-	public void setUp() throws Throwable {
-		this.deployer = new ServicesYamlParsingDeployer();
-		addDeployer( this.deployer );
-	}
-	
-	
-	/** Ensure that an empty services.yml causes no problems. */
-	@Test
-	public void testEmptyServicesYml() throws Exception {
+    private ServicesYamlParsingDeployer deployer;
+
+    @Before
+    public void setUp() throws Throwable {
+        this.deployer = new ServicesYamlParsingDeployer();
+        addDeployer( this.deployer );
+    }
+
+
+    /** Ensure that an empty services.yml causes no problems. */
+    @Test
+    public void testEmptyServicesYml() throws Exception {
         URL servicesYml = getClass().getResource("empty.yml");
-        
+
         String deploymentName = addDeployment(servicesYml, "services.yml");
         processDeployments(true);
-        
+
         DeploymentUnit unit = getDeploymentUnit( deploymentName );
-        
+
         assertNotNull( unit );
-        
-	}
-	
-	/** Ensure that a valid services.yml attaches metadata. */
+
+    }
+
+
+    /** Ensure that a valid services.yml attaches metadata. */
     @Ignore
-	@Test
-	public void testValidServicesYml() throws Exception {
+    @Test
+    public void testValidServicesYml() throws Exception {
         URL servicesYml = getClass().getResource("valid-services.yml");
-        
+
         String deploymentName = addDeployment(servicesYml, "services.yml");
         processDeployments(true);
-        
+
         DeploymentUnit unit = getDeploymentUnit( deploymentName );
-        
+
         assertNotNull( unit );
-	}
-	
+    }
+
 }
