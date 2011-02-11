94c88f55805aabf073850e46d59e9050be3dfb07
Tobias Crawley <tcrawley@gmail.com>
2011-02-11 16:50:56 -0500

    Less noise!

--- components/rack/rack-core/src/main/java/org/torquebox/rack/core/servlet/RackFilter.java
+++ components/rack/rack-core/src/main/java/org/torquebox/rack/core/servlet/RackFilter.java
@@ -76,9 +76,9 @@ public class RackFilter implements Filter {
 
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        log.info( "request: " + request );
-        log.info( "request.pathInfo: " + request.getPathInfo() );
-        log.info( "request.requestUri: " + request.getRequestURI() );
+        //log.info( "request: " + request );
+        //log.info( "request.pathInfo: " + request.getPathInfo() );
+        //log.info( "request.requestUri: " + request.getRequestURI() );
 
         if (((request.getPathInfo() == null) || (request.getPathInfo().equals( "/" ))) && !(request.getRequestURI().endsWith( "/" ))) {
             String redirectUri = request.getRequestURI() + "/";
