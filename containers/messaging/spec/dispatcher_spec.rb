ee9d9fa6d1250827379f092a2c2936619111b467
Marek Goldmann <marek.goldmann@gmail.com>
2011-02-25 23:57:52 +0100

    You can now specify naming parameters for Dispatcher, like this:
    dispatcher = TorqueBox::Messaging::Dispatcher.new(:naming_host => 'ahost', :naming_port => 1234) do
      map Nothing, Topic.new('/topics/not'), 'filter' => 'whatever'
    end

--- containers/messaging/spec/dispatcher_spec.rb
+++ containers/messaging/spec/dispatcher_spec.rb
@@ -1,4 +1,3 @@
-
 require 'torquebox/messaging/dispatcher'
 
 describe TorqueBox::Messaging::Dispatcher do
@@ -19,7 +18,7 @@ describe TorqueBox::Messaging::Dispatcher do
   it "should have correct message processor metadata" do
 
     dispatcher = TorqueBox::Messaging::Dispatcher.new do
-      map 'Nothing', '/queues/not', :filter => "x=1" 
+      map 'Nothing', '/queues/not', :filter => "x=1"
       map Nothing, Topic.new('/topics/not'), 'filter' => 'whatever'
     end
 
@@ -30,13 +29,34 @@ describe TorqueBox::Messaging::Dispatcher do
     p1.ruby_require_path.should == 'nothing'
     p1.destination_name.should == '/queues/not'
     p1.message_selector.should == 'x=1'
-    
+
     p2 = dispatcher.processors.shift
     p2.ruby_class_name.should == 'Nothing'
     p2.ruby_require_path.should be_nil
     p2.destination_name.should == '/topics/not'
     p2.message_selector.should == 'whatever'
-    
+
   end
 
+  context "naming" do
+    it "should configure naming host and url" do
+      dispatcher = TorqueBox::Messaging::Dispatcher.new(:naming_host => 'ahost', :naming_port => 1234) do
+        map Nothing, Topic.new('/topics/not'), 'filter' => 'whatever'
+      end
+
+      dispatcher.send(:container)
+
+      java.lang::System.getProperty('java.naming.provider.url').should eql('jnp://ahost:1234/')
+    end
+
+    it "should configure only naming host" do
+      dispatcher = TorqueBox::Messaging::Dispatcher.new(:naming_host => 'somehost') do
+        map Nothing, Topic.new('/topics/not'), 'filter' => 'whatever'
+      end
+
+      dispatcher.send(:container)
+
+      java.lang::System.getProperty('java.naming.provider.url').should eql('jnp://somehost:1099/')
+    end
+  end
 end
