d126a1c597de672c12ff860994af57377ee909aa
Bob McWhirter <bob@mcwhirter.org>
2011-03-01 15:58:54 -0500

    Add torquebox gem to the assembly. Add integration test showing it's sufficient for doing some messaging.

---
+++ integration-tests/spec/backgroundable_simple_gem_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'torquebox-messaging'
+
+describe "backgroundable tests" do
+
+  deploy :path => "rack/background-simple-gem-knob.yml"
+
+  before(:each) do
+    @foreground = TorqueBox::Messaging::Queue.new("/queues/foreground")
+    @background = TorqueBox::Messaging::Queue.new("/queues/background")
+  end
+
+  it "should wait asynchronously" do
+    visit "/background"
+    page.should have_content('it worked')
+    @background.publish "release"
+    result = @foreground.receive(:timeout => 25000)
+    result.should == "success"
+  end
+
+  it "should properly handle backgrounded methods on reloaded classes" do
+    visit "/background?redefine=1"
+    page.should have_content('it worked')
+    @background.publish "release"
+    result = @foreground.receive(:timeout => 25000)
+    result.should == "success"
+  end
+end
