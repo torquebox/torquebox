8b05fbf6082791efeb83d0d895dc5c8a8cb0cd03
Jim Crossley <jim@crossleys.org>
2011-03-03 17:52:34 -0500

    Rename member and reduce logging during test

--- components/web/web-core/spec/torque_box_store_spec.rb
+++ components/web/web-core/spec/torque_box_store_spec.rb
@@ -4,39 +4,39 @@ require 'logger'
 describe ActiveSupport::Cache::TorqueBoxStore do
 
   before(:each) do
-    @store = ActiveSupport::Cache::TorqueBoxStore.new()
-    @store.logger = Logger.new(STDOUT)
+    @cache = ActiveSupport::Cache::TorqueBoxStore.new()
+    # @cache.logger = Logger.new(STDOUT)
   end
 
   describe "basics" do
 
     it "should write and read a string" do
-      @store.write("key", "value").should be_true
-      @store.read("key").should == "value"
+      @cache.write("key", "value").should be_true
+      @cache.read("key").should == "value"
     end
 
     it "should write and read a number" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
     end
 
     it "should exist after writing" do
-      @store.write("key", 42).should be_true
-      @store.exist?("key").should be_true
+      @cache.write("key", 42).should be_true
+      @cache.exist?("key").should be_true
     end
 
     it "should be gone after deleting" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
-      @store.delete("key").should be_true
-      @store.read("key").should be_nil
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
+      @cache.delete("key").should be_true
+      @cache.read("key").should be_nil
     end
 
     it "should overwrite an existing key" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
-      @store.write("key", 44).should be_true
-      @store.read("key").should == 44
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
+      @cache.write("key", 44).should be_true
+      @cache.read("key").should == 44
     end
 
   end
@@ -44,25 +44,25 @@ describe ActiveSupport::Cache::TorqueBoxStore do
   describe "options" do
 
     it "should be expirable" do
-      @store.write("key", 42, :expires_in => 1.second).should be_true
-      @store.read("key").should == 42
+      @cache.write("key", 42, :expires_in => 1.second).should be_true
+      @cache.read("key").should == 42
       sleep(1.1)
-      @store.read("key").should be_nil
+      @cache.read("key").should be_nil
     end
 
     it "should optionally not overwrite an existing key" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
-      @store.write("key", 44, :unless_exist => true).should be_true
-      @store.read("key").should == 42
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
+      @cache.write("key", 44, :unless_exist => true).should be_true
+      @cache.read("key").should == 42
     end
 
     it "should merge initialized options" do
-      @store = ActiveSupport::Cache::TorqueBoxStore.new(:expires_in => 1.second)
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
+      @cache = ActiveSupport::Cache::TorqueBoxStore.new(:expires_in => 1.second)
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
       sleep(1.1)
-      @store.read("key").should be_nil
+      @cache.read("key").should be_nil
     end
 
   end
@@ -70,22 +70,22 @@ describe ActiveSupport::Cache::TorqueBoxStore do
   describe "fetching" do
 
     it "should fetch existing keys" do
-      @store.write("today", "Monday")
-      @store.fetch("today").should == "Monday"
+      @cache.write("today", "Monday")
+      @cache.fetch("today").should == "Monday"
     end
 
     it "should fetch block values for missing keys" do
-      @store.fetch("city").should be_nil
-      @store.fetch("city") {
+      @cache.fetch("city").should be_nil
+      @cache.fetch("city") {
         "Duckburgh"
       }.should == "Duckburgh"
-      @store.fetch("city").should == "Duckburgh"
+      @cache.fetch("city").should == "Duckburgh"
     end
 
     it "should fetch block values when forced" do
-      @store.write("today", "Monday")
-      @store.fetch("today").should == "Monday"
-      @store.fetch("today", :force => true) { "Tuesday" }.should == "Tuesday"
+      @cache.write("today", "Monday")
+      @cache.fetch("today").should == "Monday"
+      @cache.fetch("today", :force => true) { "Tuesday" }.should == "Tuesday"
     end
 
   end
@@ -93,39 +93,39 @@ describe ActiveSupport::Cache::TorqueBoxStore do
   describe "multiples" do
 
     before(:each) do
-      @store.write("john", "guitar")
-      @store.write("paul", "bass")
-      @store.write("george", "lead")
-      @store.write("ringo", "drums")
+      @cache.write("john", "guitar")
+      @cache.write("paul", "bass")
+      @cache.write("george", "lead")
+      @cache.write("ringo", "drums")
     end
 
     it "should delete by regexp" do
-      @store.delete_matched /g/
-      @store.read("george").should be_nil
-      @store.read("ringo").should be_nil
-      @store.read("john").should == "guitar"
-      @store.read("paul").should == "bass"
+      @cache.delete_matched /g/
+      @cache.read("george").should be_nil
+      @cache.read("ringo").should be_nil
+      @cache.read("john").should == "guitar"
+      @cache.read("paul").should == "bass"
     end
 
     it "should clear all entries" do
-      @store.clear
-      @store.read("george").should be_nil
-      @store.read("ringo").should be_nil
-      @store.read("john").should be_nil
-      @store.read("paul").should be_nil
+      @cache.clear
+      @cache.read("george").should be_nil
+      @cache.read("ringo").should be_nil
+      @cache.read("john").should be_nil
+      @cache.read("paul").should be_nil
     end
 
     it "should cleanup expired entries" do
-      @store.write("jimi", "guitar", :expires_in => 1.second)
-      @store.exist?("jimi").should be_true
+      @cache.write("jimi", "guitar", :expires_in => 1.second)
+      @cache.exist?("jimi").should be_true
       sleep(1.1)
-      @store.cleanup
-      @store.exist?("jimi").should be_false
-      @store.exist?("ringo").should be_true
+      @cache.cleanup
+      @cache.exist?("jimi").should be_false
+      @cache.exist?("ringo").should be_true
     end
 
     it "should read multiple values" do
-      @store.read_multi("john", "paul").should == {"john" => "guitar", "paul" => "bass"}
+      @cache.read_multi("john", "paul").should == {"john" => "guitar", "paul" => "bass"}
     end
 
   end
@@ -133,17 +133,17 @@ describe ActiveSupport::Cache::TorqueBoxStore do
   describe "advanced" do
     
     it "should support incrementation" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
-      @store.increment("key").should == 43
-      @store.read("key").should == 43
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
+      @cache.increment("key").should == 43
+      @cache.read("key").should == 43
     end
 
     it "should support decrementation" do
-      @store.write("key", 42).should be_true
-      @store.read("key").should == 42
-      @store.decrement("key").should == 41
-      @store.read("key").should == 41
+      @cache.write("key", 42).should be_true
+      @cache.read("key").should == 42
+      @cache.decrement("key").should == 41
+      @cache.read("key").should == 41
     end
 
   end
