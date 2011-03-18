a2fab77fbd5099e1d3dd4fe207629a009331324d
Bob McWhirter <bob@mcwhirter.org>
2011-03-17 21:10:30 -0400

    Remove now-unused branch of the analysis visitor.

--- components/base/base-int/src/main/java/org/torquebox/injection/InjectBlockVisitor.java
+++ components/base/base-int/src/main/java/org/torquebox/injection/InjectBlockVisitor.java
@@ -9,33 +9,17 @@ import org.jruby.ast.SymbolNode;
 import org.torquebox.interp.analysis.DefaultNodeVisitor;
 
 public class InjectBlockVisitor extends DefaultNodeVisitor {
-    
+
     public InjectBlockVisitor(InjectionAnalyzer analyzer) {
         this.analyzer = analyzer;
     }
 
     @Override
     public Object visitFCallNode(FCallNode node) {
-        if (node.getName().equals( "inject" )) {
-            Node genericArgsNode = node.getArgsNode();
-
-            if (genericArgsNode instanceof ArrayNode) {
-                ArrayNode argsNode = (ArrayNode) genericArgsNode;
-
-                if (argsNode.size() == 2) {
-                    Node siteNode = argsNode.get( 0 );
-                    Node injectableNode = argsNode.get( 1 );
-
-                    Injectable injectable = (Injectable) injectableNode.accept(  this  );
-                    return injectable;
-                }
-            }
-        } else {
-            String type = node.getName();
-            InjectableHandler handler = this.analyzer.getInjectableHandlerRegistry().getHandler( type );
-            if ( handler != null ) {
-                return handler.handle(  node.getArgsNode() );
-            }
+        String type = node.getName();
+        InjectableHandler handler = this.analyzer.getInjectableHandlerRegistry().getHandler( type );
+        if (handler != null) {
+            return handler.handle( node.getArgsNode() );
         }
 
         return null;
@@ -48,15 +32,14 @@ public class InjectBlockVisitor extends DefaultNodeVisitor {
             str = ((StrNode) node).getValue().toString();
         } else if (node.getNodeType() == NodeType.SYMBOLNODE) {
             str = ((SymbolNode) node).getName();
-        } else if ( node.getNodeType() == NodeType.ARRAYNODE ) {
-            if ( ((ArrayNode)node) .size() == 1 ) {
-                str = getString( ((ArrayNode)node).get( 0 ) );
+        } else if (node.getNodeType() == NodeType.ARRAYNODE) {
+            if (((ArrayNode) node).size() == 1) {
+                str = getString( ((ArrayNode) node).get( 0 ) );
             }
         }
         return str;
     }
 
     private InjectionAnalyzer analyzer;
-
 
 }
