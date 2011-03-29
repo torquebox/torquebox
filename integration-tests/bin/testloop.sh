34f4bda3424cce70e049a6e03c874f90ce04e741
Bob McWhirter <bob@mcwhirter.org>
2011-03-29 14:51:32 -0400

    Helper script for beating the crap out of a test.

---
+++ integration-tests/bin/testloop.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+i=0
+
+control_c() 
+{
+  echo "Exiting"
+  exit
+}
+
+trap control_c SIGINT
+
+echo "Looping test: $*"
+
+for (( ; ; ))
+do
+  i=$((i+1))
+  echo "Loop $i"
+  mvn test -Dtest=*$*
+  if [ $? != 0 ] 
+  then
+    echo "Failure!"   
+    exit
+  fi
+done
