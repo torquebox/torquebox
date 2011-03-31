05d2f17332217ad4256dcae154ee37cd33d82219
Bob McWhirter <bob@mcwhirter.org>
2011-03-31 09:27:26 -0400

    Bugs!

--- build-support/bees-ci-build.sh
+++ build-support/bees-ci-build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+echo "*** Unsetting variables ***"
+echo "Before:"
+echo "     GEM_HOME: $GEM_HOME"
+echo "     GEM_PATH: $GEM_PATH"
+echo "  BUNDLE_PATH: $BUNDLE_PATH"
+
+unset GEM_HOME
+unset GEM_PATH
+unset BUNDLE_PATH
+
+echo "After:"
+echo "     GEM_HOME: $GEM_HOME"
+echo "     GEM_PATH: $GEM_PATH"
+echo "  BUNDLE_PATH: $BUNDLE_PATH"
+
+echo "*** Environment ***"
+set
+
+echo "*** PWD ***"
+echo $PWD
+
 echo "*** Start Preflight ***"
 echo "Removing $M2_REPO/org/torquebox/"
 rm -rf $M2_REPO/org/torquebox
@@ -12,6 +33,9 @@ $MAVEN_HOME/bin/mvn clean -Pinteg -Pdist
 
 echo "*** Start Build ***"
 
-echo "Peforming build"
-$MAVEN_HOME/bin/mvn -Dmaven.repo.local=$M2_REPO -U -s build-support/settings.xml install -Pinteg -Pdist -Pci -e
+echo "Peforming core build skipping tests"
+$MAVEN_HOME/bin/mvn -Dmaven.repo.local=$M2_REPO -U -s build-support/settings.xml install -Dmaven.test.skip=true
+
+echo "Peforming integ build"
+cd integration-tests && $MAVEN_HOME/bin/mvn -Dmaven.repo.local=$M2_REPO -U -s build-support/settings.xml test
 
