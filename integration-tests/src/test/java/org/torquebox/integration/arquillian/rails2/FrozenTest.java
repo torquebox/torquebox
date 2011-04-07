005086a363de65b97629871b4f4d12b205debb7c
Tobias Crawley <tcrawley@gmail.com>
2011-04-07 13:46:03 -0400

    Fixed up integs for 1.9.
    Now that container.rb isn't responsible for running apps in 1.9 mode, we can
    run any junit tested apps under 1.9 as well, though a few of the apps needed
    to be updated to work under 1.9.

--- integration-tests/src/test/java/org/torquebox/integration/arquillian/rails2/FrozenTest.java
+++ integration-tests/src/test/java/org/torquebox/integration/arquillian/rails2/FrozenTest.java
@@ -45,8 +45,8 @@ public class FrozenTest extends AbstractIntegrationTestCase {
     @Test
     public void testHighLevel() {
         driver.get( "http://localhost:8080/frozen-rails" );
-        // System.err.println("RESULT: ");
-        // System.err.println(driver.getPageSource());
+        //System.err.println("RESULT: ");
+        //System.err.println(driver.getPageSource());
         WebElement element = driver.findElementById( "success" );
         assertNotNull( element );
         assertEquals( "frozen-rails", element.getAttribute( "class" ) );
