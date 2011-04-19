f59e1d83e5be45bbe7a12920fad9ea003b404adf
Jim Crossley <jim@crossleys.org>
2011-04-18 23:04:28 -0400

    Ported rails2/BasicTest over to rspec, including the -rails compat stuff in the same file.

--- integration-tests/spec/basic_rails2_spec.rb
+++ integration-tests/spec/basic_rails2_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
-describe "basic backwards compatibility" do
-
-  deploy "rails2/basic-rails.yml"
+shared_examples_for "basic rails2 tests" do
 
   it "should return a basic page" do
     visit "/basic-rails"
@@ -21,4 +19,13 @@ describe "basic backwards compatibility" do
     page.source.chomp.should == "this is the contents of the file"
   end
 
+end
+
+describe "basic backwards compatibility" do
+  deploy "rails2/basic-rails.yml"
+  it_should_behave_like "basic rails2 tests"
+end
+describe "basic knob compatibility" do
+  deploy "rails2/basic-knob.yml"
+  it_should_behave_like "basic rails2 tests"
 end
