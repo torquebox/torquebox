212d83f2957d2ae3bda241316db5e8570f9c8fd2
Jim Crossley <jim@crossleys.org>
2011-04-19 14:09:30 -0400

    Ported InjectionTest and consolidated the "rails3/basic-knob.yml" deploys into one spec.

--- integration-tests/spec/basic_rails3_spec.rb
+++ integration-tests/spec/basic_rails3_spec.rb
@@ -4,10 +4,23 @@ describe "basic rack test" do
 
   deploy "rails3/basic-knob.yml"
 
-  it "should work" do
+  it "should do a basic get" do
     visit "/basic-rails"
     page.should have_content('It works')
     page.find("#success")[:class].should == 'basic-rails'
+  end
+
+  it "should do a raw post" do
+    visit "/basic-rails/post/raw"
+    fill_in "name", :with => "my-name"
+    click_button "submit"
+    find("#raw_post").should have_content("name=my-name")
+    find("#name").should have_content("my-name")
+  end
+
+  it "should support injection" do
+    visit "/basic-rails/root/injectiontest"
+    find('#success').text.should == 'taco'
   end
 
 end
