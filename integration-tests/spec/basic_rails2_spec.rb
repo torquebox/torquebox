a461716e0de3384f9c1c363dc989944abecfbeba
Jim Crossley <jim@crossleys.org>
2011-04-19 10:45:02 -0400

    Integrated basic archive tests into the basic_rails2_spec.

--- integration-tests/spec/basic_rails2_spec.rb
+++ integration-tests/spec/basic_rails2_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
+require 'fileutils'
+require 'torquebox/deploy_utils'
+
+knob_path = TorqueBox::DeployUtils.create_archive( "basic-rails2.knob", 
+                                                   File.join( File.dirname( __FILE__ ), "../apps/rails2/basic" ),
+                                                   File.join( File.dirname( __FILE__ ), "../target/" ) )
+old_path = knob_path.sub(/knob$/, 'rails')
+FileUtils.cp( knob_path, old_path )
 
 shared_examples_for "basic rails2 tests" do
 
@@ -27,5 +35,13 @@ describe "basic backwards compatibility" do
 end
 describe "basic knob compatibility" do
   deploy "rails2/basic-knob.yml"
+  it_should_behave_like "basic rails2 tests"
+end
+describe "basic archive backwards compatibility" do
+  deploy old_path
+  it_should_behave_like "basic rails2 tests"
+end
+describe "basic archive knob compatibility" do
+  deploy knob_path
   it_should_behave_like "basic rails2 tests"
 end
