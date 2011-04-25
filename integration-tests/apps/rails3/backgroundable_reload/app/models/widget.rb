e279d5ba494c191af7ba0ad3742bf3b4322f3830
Jim Crossley <jim@crossleys.org>
2011-04-25 11:57:42 -0400

    Consolidated reload specs into a shared one, bumped up sleep time, but eliminated one of the rewrites.

--- integration-tests/apps/rails3/backgroundable_reload/app/models/widget.rb
+++ integration-tests/apps/rails3/backgroundable_reload/app/models/widget.rb
@@ -3,7 +3,7 @@ class Widget < ActiveRecord::Base
     @queue = TorqueBox::Messaging::Queue.new('/queues/background')
     @queue.publish('a response')
     puts "published 'a response'"
-    sleep(2)
+    sleep(3)
   end
   always_background :foo
 end
