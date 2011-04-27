c0e1da6085b51cd19c43646989605afc283ac5e1
Jim Crossley <jim@crossleys.org>
2011-04-27 10:44:13 -0400

    Reverting TORQUE-372 preload fix as it causes version conflicts for rails 2 apps.

--- components/base/base-gem/lib/gem_hook.rb
+++ components/base/base-gem/lib/gem_hook.rb
@@ -3,9 +3,3 @@ require 'torquebox/kernel'
 require 'torquebox/component_manager'
 require 'torquebox/injectors'
 require 'torquebox/logger'
-
-begin
-  require 'active_support/cache/torque_box_store'
-rescue Exception
-  # pre-loadable only if activesupport is in $:
-end
