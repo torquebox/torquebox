967ccff4389e2449205d3a11f702a6186d3f1e76
Jim Crossley <jim@crossleys.org>
2011-04-30 08:39:10 -0400

    Good example of the three different deploy options.

--- integration-tests/spec/basic_rack_spec.rb
+++ integration-tests/spec/basic_rack_spec.rb
@@ -1,8 +1,44 @@
 require 'spec_helper'
 
-describe "basic rack test" do
+describe "basic rack test with filename" do
 
   deploy "rack/basic-knob.yml"
+
+  it "should work" do
+    visit "/basic-rack"
+    page.should have_content('it worked')
+    page.find("#success")[:class].should == 'basic-rack'
+  end
+
+end
+
+describe "basic rack test with heredoc" do
+
+  deploy <<-END.gsub(/^ {4}/,'')
+
+    application:
+      root: #{File.dirname(__FILE__)}/../apps/rack/basic
+      env: development
+    web:
+      context: /basic-rack
+    ruby:
+      version: #{RUBY_VERSION[0,3]}
+
+  END
+
+  it "should work" do
+    visit "/basic-rack"
+    page.should have_content('it worked')
+    page.find("#success")[:class].should == 'basic-rack'
+  end
+
+end
+
+describe "basic rack test with hash" do
+
+  deploy( :application => { :root => "#{File.dirname(__FILE__)}/../apps/rack/basic", :env => 'development' },
+          :web => { :context => '/basic-rack' },
+          :ruby => { :version => RUBY_VERSION } )  
 
   it "should work" do
     visit "/basic-rack"
