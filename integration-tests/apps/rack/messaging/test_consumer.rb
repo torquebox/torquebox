eea2873fa4566ed1f8da6e790087c71a86480507
Tobias Crawley <tcrawley@gmail.com>
2011-05-11 11:34:07 -0400

    Replaced rails2 with rack for messaging_spec [TORQUE-389]

--- integration-tests/apps/rack/messaging/test_consumer.rb
+++ integration-tests/apps/rack/messaging/test_consumer.rb
@@ -1,6 +1,9 @@
+require 'torquebox-messaging'
+
 class TestConsumer < TorqueBox::Messaging::MessageProcessor
   
   def on_message(body)
+    puts "on_message: #{body}"
     TorqueBox::Messaging::Queue.new('/queues/results').publish( "result=#{body}" )
   end
   
