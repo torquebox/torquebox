94a1a4833aa79943e600a7112042f4857309d5f0
Tobias Crawley <tcrawley@gmail.com>
2011-05-11 15:28:01 -0400

    A little more test cleanup whilst I'm in there.

--- integration-tests/spec/task_concurrency_spec.rb
+++ integration-tests/spec/task_concurrency_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 require 'torquebox-messaging'
+require 'set'
 
 describe "task concurrency" do
 
   deploy "rack/task-concurrency-knob.yml"
 
   shared_examples_for "concurrent processors" do
-
     before(:each) do
       @backchannel = TorqueBox::Messaging::Queue.new("/queues/backchannel")
     end
@@ -15,8 +15,11 @@ describe "task concurrency" do
       path = "/task-concurrency?#{@type}" 
       visit path
       page.should have_content('tasks fired')
-      responses = task_responses
-      responses.uniq.size.should > 1
+      responses = Set.new
+      20.times do
+        responses << @backchannel.receive(:timeout => 120_000)
+      end
+      responses.size.should > 1
     end
   end
 
@@ -33,12 +36,5 @@ describe "task concurrency" do
     end
     it_should_behave_like "concurrent processors"
   end
-  
-  def task_responses
-    response = []
-    20.times do
-      response << @backchannel.receive(:timeout => 120_000)
-    end
-    response
-  end
+
 end
