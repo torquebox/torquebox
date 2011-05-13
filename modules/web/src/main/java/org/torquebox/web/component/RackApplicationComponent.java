905e27d4345886684f522d05a60cce349d46fec2
Bob McWhirter <bob@mcwhirter.org>
2011-05-13 01:04:01 -0400

    Pass the RubyHash, not the RackEnv wrapper itself.

--- modules/web/src/main/java/org/torquebox/web/component/RackApplicationComponent.java
+++ modules/web/src/main/java/org/torquebox/web/component/RackApplicationComponent.java
@@ -4,7 +4,6 @@ import org.jruby.runtime.builtin.IRubyObject;
 import org.torquebox.core.component.AbstractRubyComponent;
 import org.torquebox.web.rack.RackEnvironment;
 import org.torquebox.web.rack.RackResponse;
-import org.torquebox.web.rack.RackResponse;
 
 public class RackApplicationComponent extends AbstractRubyComponent {
     
@@ -13,7 +12,7 @@ public class RackApplicationComponent extends AbstractRubyComponent {
     }
     
     public RackResponse call(RackEnvironment env) {
-        return new RackResponse( (IRubyObject) __call__( "call", env ) );
+        return new RackResponse( (IRubyObject) __call__( "call", env.getEnv() ) );
     }
 
 }
