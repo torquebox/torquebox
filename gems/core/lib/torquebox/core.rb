67e77975c9bf712de35d92e0c537d95426136755
Lance Ball <lball@redhat.com>
2011-05-18 16:32:01 -0400

    Add TorqueBox::ServiceRegistry stub

--- gems/core/lib/torquebox/core.rb
+++ gems/core/lib/torquebox/core.rb
@@ -3,6 +3,7 @@ module TorqueBox
 end
 
 #require 'torquebox/kernel'
+require 'torquebox/service_registry'
 require 'torquebox/component_manager'
 require 'torquebox/injectors'
 require 'torquebox/logger'
