d51dcd65fce7c6cb1c53dbe5b15d8e1c646b28c3
Jim Crossley <jim@crossleys.org>
2011-05-19 20:58:46 -0400

    Revive backgroundable_spec [TORQUE-402]

--- gems/messaging/lib/torquebox/messaging/backgroundable.rb
+++ gems/messaging/lib/torquebox/messaging/backgroundable.rb
@@ -15,7 +15,8 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-require 'torquebox/messaging/destination'
+require 'torquebox/messaging/queue'
+require 'torquebox/injectors'
 
 module TorqueBox
   module Messaging
