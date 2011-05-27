7480b0363986973ab85340b1d0ac1c8073ea4792
Jim Crossley <jim@crossleys.org>
2011-05-27 16:50:43 -0400

    Whitespace only

--- integration-tests/setup-integ-dist.rb
+++ integration-tests/setup-integ-dist.rb
@@ -15,20 +15,20 @@ else
       puts "*** Syncing to integ-dist by copy"
       if ( File.exists?( output_dir ) )
         puts "*** Only copying deployers into default profile"
-	deployer_path = 'jboss/server/default/deployers/torquebox.deployer' 
-	FileUtils.rm_rf( "#{output_dir}/#{deployer_path}" )
+        deployer_path = 'jboss/server/default/deployers/torquebox.deployer' 
+        FileUtils.rm_rf( "#{output_dir}/#{deployer_path}" )
         FileUtils.cp_r( "./#{deployer_path}", "#{output_dir}/#{deployer_path}" )
 
-	tb_common_path = 'jboss/common/torquebox'
-	FileUtils.rm_rf( "#{output_dir}/#{tb_common_path}" )
+        tb_common_path = 'jboss/common/torquebox'
+        FileUtils.rm_rf( "#{output_dir}/#{tb_common_path}" )
         FileUtils.cp_r( "./#{tb_common_path}", "#{output_dir}/#{tb_common_path}" )
 
-	jboss_lib_path = 'jboss/common/lib'
-	FileUtils.rm( Dir["#{output_dir}/#{jboss_lib_path}/torquebox*"] )
+        jboss_lib_path = 'jboss/common/lib'
+        FileUtils.rm( Dir["#{output_dir}/#{jboss_lib_path}/torquebox*"] )
 
         Dir[ "./#{jboss_lib_path}/torquebox*" ].each do |f|
-	  FileUtils.cp( f, "#{output_dir}/#{jboss_lib_path}/" )
-	end
+          FileUtils.cp( f, "#{output_dir}/#{jboss_lib_path}/" )
+        end
       else
         FileUtils.rm_rf( output_dir )
         FileUtils.cp_r( '.', output_dir )
@@ -36,19 +36,19 @@ else
     else
       puts "*** Syncing to integ-dist by rsync"
       cmd = [ 'rsync -a . --relative',
-	      '--include jboss/modules',
-	      '--include jboss/standalone',
+              '--include jboss/modules',
+              '--include jboss/standalone',
               #"--exclude 'apps/*'",
-	      #'--exclude default/deploy/*.yml',
-	      #'--exclude default/deploy/*.knob',
-	      #'--exclude default/data',
-	      #'--exclude default/work',
-	      #'--exclude default/log',
-	      #'--exclude default/tmp',
-	      #'--exclude jboss/server/*',
-	      #'--exclude jruby/share/ri',
-	      #'--exclude jruby/lib/ruby/gems/1.8/doc',
-	      output_dir ].join( ' ' )
+              #'--exclude default/deploy/*.yml',
+              #'--exclude default/deploy/*.knob',
+              #'--exclude default/data',
+              #'--exclude default/work',
+              #'--exclude default/log',
+              #'--exclude default/tmp',
+              #'--exclude jboss/server/*',
+              #'--exclude jruby/share/ri',
+              #'--exclude jruby/lib/ruby/gems/1.8/doc',
+              output_dir ].join( ' ' )
       puts cmd
       puts `#{cmd}` 
     end
