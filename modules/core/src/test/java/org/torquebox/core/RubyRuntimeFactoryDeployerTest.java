6c2878c240fa9185a033e89740dc584d97316cf1
Bob McWhirter <bob@mcwhirter.org>
2011-05-26 20:54:45 -0400

    Port runtime factory deployer test.

---
+++ modules/core/src/test/java/org/torquebox/core/RubyRuntimeFactoryDeployerTest.java
@@ -0,0 +1,163 @@
+package org.torquebox.core;
+
+import static org.junit.Assert.*;
+
+import org.jboss.as.server.deployment.Attachments;
+import org.jboss.modules.Module;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.value.Value;
+import org.jruby.CompatVersion;
+import org.jruby.RubyInstanceConfig.CompileMode;
+import org.junit.Before;
+import org.junit.Test;
+import org.torquebox.core.app.RubyApplicationMetaData;
+import org.torquebox.core.as.CoreServices;
+import org.torquebox.core.runtime.RubyRuntimeFactory;
+import org.torquebox.core.runtime.RubyRuntimeFactoryDeployer;
+import org.torquebox.core.runtime.RubyRuntimeMetaData;
+import org.torquebox.test.as.AbstractDeploymentProcessorTestCase;
+import org.torquebox.test.as.MockDeploymentPhaseContext;
+import org.torquebox.test.as.MockDeploymentUnit;
+import org.torquebox.test.as.MockServiceBuilder;
+
+public class RubyRuntimeFactoryDeployerTest extends AbstractDeploymentProcessorTestCase {
+    
+
+    @Before
+    public void setUpDeployer() throws Throwable {
+        appendDeployer( new RubyRuntimeFactoryDeployer() );
+    }
+
+    @Test
+    public void testDeployment() throws Exception {
+        
+        MockDeploymentPhaseContext phaseContext = createPhaseContext();
+        MockDeploymentUnit unit = phaseContext.getMockDeploymentUnit();
+
+        RubyRuntimeMetaData metaData = new RubyRuntimeMetaData();
+        unit.putAttachment( RubyRuntimeMetaData.ATTACHMENT_KEY, metaData );
+        
+        RubyApplicationMetaData rubyAppMetaData = new RubyApplicationMetaData( "foo" );
+        unit.putAttachment( RubyApplicationMetaData.ATTACHMENT_KEY, rubyAppMetaData );
+        
+        unit.putAttachment( Attachments.MODULE, Module.getSystemModule() );
+
+        deploy( phaseContext );
+
+        ServiceName factoryServiceName = CoreServices.runtimeFactoryName( unit );
+        MockServiceBuilder<?> factoryBuilder = phaseContext.getMockServiceTarget().getMockServiceBuilder( factoryServiceName );
+        Value<?> factoryValue = factoryBuilder.getValue();
+        RubyRuntimeFactory factory = (RubyRuntimeFactory) factoryValue.getValue();
+
+        assertNotNull( factory );
+        assertEquals( CompatVersion.RUBY1_8, factory.getRubyVersion() );
+    }
+
+    @Test
+    public void testDeploymentWithNonDefaultRubyCompatibilityVersion() throws Exception {
+        
+        MockDeploymentPhaseContext phaseContext = createPhaseContext();
+        MockDeploymentUnit unit = phaseContext.getMockDeploymentUnit();
+        
+        RubyRuntimeMetaData metaData = new RubyRuntimeMetaData();
+        metaData.setVersion( RubyRuntimeMetaData.Version.V1_9 );
+        unit.putAttachment( RubyRuntimeMetaData.ATTACHMENT_KEY, metaData );
+        
+        RubyApplicationMetaData rubyAppMetaData = new RubyApplicationMetaData( "foo");
+        unit.putAttachment( RubyApplicationMetaData.ATTACHMENT_KEY, rubyAppMetaData );
+        
+        unit.putAttachment( Attachments.MODULE, Module.getSystemModule() );
+
+        deploy( phaseContext );
+
+        ServiceName factoryServiceName = CoreServices.runtimeFactoryName( unit );
+        MockServiceBuilder<?> factoryBuilder = phaseContext.getMockServiceTarget().getMockServiceBuilder( factoryServiceName );
+        Value<?> factoryValue = factoryBuilder.getValue();
+        RubyRuntimeFactory factory = (RubyRuntimeFactory) factoryValue.getValue();
+
+        assertNotNull( factory );
+        assertEquals( CompatVersion.RUBY1_9, factory.getRubyVersion() );
+    }
+
+    @Test
+    public void testDeploymentWithJITCompileMode() throws Exception {
+        MockDeploymentPhaseContext phaseContext = createPhaseContext();
+        MockDeploymentUnit unit = phaseContext.getMockDeploymentUnit();
+        
+        RubyRuntimeMetaData metaData = new RubyRuntimeMetaData();
+        metaData.setCompileMode( RubyRuntimeMetaData.CompileMode.JIT );
+        unit.putAttachment( RubyRuntimeMetaData.ATTACHMENT_KEY, metaData );
+
+
+        RubyApplicationMetaData rubyAppMetaData = new RubyApplicationMetaData( "foo");
+        unit.putAttachment( RubyApplicationMetaData.ATTACHMENT_KEY, rubyAppMetaData );
+        
+        unit.putAttachment( Attachments.MODULE, Module.getSystemModule() );
+
+        deploy( phaseContext );
+
+        ServiceName factoryServiceName = CoreServices.runtimeFactoryName( unit );
+        MockServiceBuilder<?> factoryBuilder = phaseContext.getMockServiceTarget().getMockServiceBuilder( factoryServiceName );
+        Value<?> factoryValue = factoryBuilder.getValue();
+        RubyRuntimeFactory factory = (RubyRuntimeFactory) factoryValue.getValue();
+
+        assertNotNull( factory );
+        assertEquals( CompileMode.JIT, factory.getCompileMode() );
+
+    }
+
+    @Test
+    public void testDeploymentWithOFFCompileMode() throws Exception {
+        MockDeploymentPhaseContext phaseContext = createPhaseContext();
+        MockDeploymentUnit unit = phaseContext.getMockDeploymentUnit();
+        
+        RubyRuntimeMetaData metaData = new RubyRuntimeMetaData();
+        metaData.setCompileMode( RubyRuntimeMetaData.CompileMode.OFF );
+        unit.putAttachment( RubyRuntimeMetaData.ATTACHMENT_KEY, metaData );
+
+
+        RubyApplicationMetaData rubyAppMetaData = new RubyApplicationMetaData( "foo");
+        unit.putAttachment( RubyApplicationMetaData.ATTACHMENT_KEY, rubyAppMetaData );
+        
+        unit.putAttachment( Attachments.MODULE, Module.getSystemModule() );
+
+        deploy( phaseContext );
+
+        ServiceName factoryServiceName = CoreServices.runtimeFactoryName( unit );
+        MockServiceBuilder<?> factoryBuilder = phaseContext.getMockServiceTarget().getMockServiceBuilder( factoryServiceName );
+        Value<?> factoryValue = factoryBuilder.getValue();
+        RubyRuntimeFactory factory = (RubyRuntimeFactory) factoryValue.getValue();
+
+        assertNotNull( factory );
+        assertEquals( CompileMode.OFF, factory.getCompileMode() );
+    }
+
+    @Test
+    public void testDeploymentWithFORCECompileMode() throws Exception {
+        MockDeploymentPhaseContext phaseContext = createPhaseContext();
+        MockDeploymentUnit unit = phaseContext.getMockDeploymentUnit();
+        
+        RubyRuntimeMetaData metaData = new RubyRuntimeMetaData();
+        metaData.setCompileMode( RubyRuntimeMetaData.CompileMode.FORCE );
+        unit.putAttachment( RubyRuntimeMetaData.ATTACHMENT_KEY, metaData );
+
+
+        RubyApplicationMetaData rubyAppMetaData = new RubyApplicationMetaData( "foo");
+        unit.putAttachment( RubyApplicationMetaData.ATTACHMENT_KEY, rubyAppMetaData );
+        
+        unit.putAttachment( Attachments.MODULE, Module.getSystemModule() );
+
+        deploy( phaseContext );
+
+        ServiceName factoryServiceName = CoreServices.runtimeFactoryName( unit );
+        MockServiceBuilder<?> factoryBuilder = phaseContext.getMockServiceTarget().getMockServiceBuilder( factoryServiceName );
+        Value<?> factoryValue = factoryBuilder.getValue();
+        RubyRuntimeFactory factory = (RubyRuntimeFactory) factoryValue.getValue();
+
+        assertNotNull( factory );
+        assertEquals( CompileMode.FORCE, factory.getCompileMode() );
+    }
+
+
+
+}
