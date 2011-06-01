63538fa180bee2a0c3eb581e5bbf71cea3e10d87
Bob McWhirter <bob@mcwhirter.org>
2011-06-01 15:01:35 -0400

    Remove verbose from rsync. Add docs to build. Prepare a bees-based staging deployment profile.

--- integration-tests/setup-integ-dist.rb
+++ integration-tests/setup-integ-dist.rb
@@ -35,7 +35,7 @@ else
       end
     else
       puts "*** Syncing to integ-dist by rsync"
-      cmd = [ 'rsync -av . --relative',
+      cmd = [ 'rsync -a . --relative',
               '--include jboss/modules',
               '--include jboss/standalone',
               '--exclude jruby/share/ri',
