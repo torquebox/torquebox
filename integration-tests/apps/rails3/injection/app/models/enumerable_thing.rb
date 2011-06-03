40a8d4e01b4db1609da7ff45507a60345dcebeae
Tobias Crawley <tcrawley@gmail.com>
2011-06-03 17:53:21 -0400

    Oops, the integs just might fail w/o this [TORQUE-435]

---
+++ integration-tests/apps/rails3/injection/app/models/enumerable_thing.rb
@@ -0,0 +1,15 @@
+class EnumerableThing
+  include TorqueBox::Injectors
+  include Enumerable
+  
+  def initialize
+    @hash = { :a => :b }
+    queue = __inject__('/queues/injection_enumerable')
+
+    inject('this should not barf') { |_, __| queue.publish('it worked') }
+  end
+
+  def each(&block)
+    @hash.each(&block)
+  end
+end
