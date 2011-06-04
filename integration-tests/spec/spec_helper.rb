64d406060358dc3fb9b8b8c441e0f16a24434e13
Jim Crossley <jim@crossleys.org>
2011-06-03 22:00:40 -0400

    Split the helper up into what's needed on the client and what's needed on the server.

--- integration-tests/spec/spec_helper.rb
+++ integration-tests/spec/spec_helper.rb
@@ -1,8 +1,5 @@
 require 'torquespec'
-require 'capybara/dsl'
-require 'akephalos'
 require 'fileutils'
-require 'jmx4r'
 
 TorqueSpec.jboss_home = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'integ-dist', 'jboss' ) )
 TorqueSpec.max_heap = java.lang::System.getProperty( 'max.heap' )
@@ -11,21 +8,6 @@ TorqueSpec.jvm_args += " -Djruby.home=#{java.lang::System.getProperty( 'jruby.ho
 
 TorqueSpec.knob_root = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'knobs' ) )
 FileUtils.mkdir_p(TorqueSpec.knob_root) unless File.exist?(TorqueSpec.knob_root)
-
-Capybara.register_driver :akephalos do |app|
-  Capybara::Driver::Akephalos.new(app, :browser => :firefox_3)
-end
-
-Capybara.default_driver = :akephalos
-Capybara.app_host = "http://localhost:8080"
-Capybara.run_server = false
-
-RSpec.configure do |config|
-  config.include Capybara
-  config.after do
-    Capybara.reset_sessions!
-  end
-end
 
 MUTABLE_APP_BASE_PATH  = File.join( File.dirname( __FILE__ ), '..', 'target', 'apps' )
 TESTING_ON_WINDOWS = ( java.lang::System.getProperty( "os.name" ) =~ /windows/i )
@@ -38,32 +20,6 @@ def mutable_app(path)
   FileUtils.cp_r( File.join( File.dirname( __FILE__ ), '..', 'apps', path ), dest_path )
 end
 
-def add_request_header(key, value)
-  page.driver.browser.send(:client).add_request_header(key, value)
-end
-
-def mbean(name)
-  JMX::MBean.establish_connection :command => /org.jboss.as.standalone/i
-  JMX::MBean.find_by_name(name)
-end
-
-def get_msc_service_state(service_name)
-  @msc ||= mbean('jboss.msc:type=container,name=jboss-as')
-  @msc.getServiceStatus(service_name).get('stateName')
-end
-
-def set_msc_service_mode(service_name, mode)
-  @msc ||= mbean('jboss.msc:type=container,name=jboss-as')
-  @msc.setServiceMode(service_name, mode)
-end
-
-def verify_msc_service_state(service_name, state, options={})
-  options[:timeout] ||= 30 # default to wait 30 seconds
-  service_state = nil
-  elapsed_seconds = 0
-  until service_state == state || elapsed_seconds > options[:timeout] do
-    service_state = get_msc_service_state(service_name)
-    sleep(0.5)
-  end
-  service_state.should == state
+unless ENV['TORQUEBOX_APP_NAME']
+  require 'spec_helper_integ'
 end
---
+++ integration-tests/spec/spec_helper_integ.rb
@@ -0,0 +1,48 @@
+require 'capybara/dsl'
+require 'akephalos'
+require 'jmx4r'
+
+Capybara.register_driver :akephalos do |app|
+  Capybara::Driver::Akephalos.new(app, :browser => :firefox_3)
+end
+
+Capybara.default_driver = :akephalos
+Capybara.app_host = "http://localhost:8080"
+Capybara.run_server = false
+
+RSpec.configure do |config|
+  config.include Capybara
+  config.after do
+    Capybara.reset_sessions!
+  end
+end
+
+def add_request_header(key, value)
+  page.driver.browser.send(:client).add_request_header(key, value)
+end
+
+def mbean(name)
+  JMX::MBean.establish_connection :command => /org.jboss.as.standalone/i
+  JMX::MBean.find_by_name(name)
+end
+
+def get_msc_service_state(service_name)
+  @msc ||= mbean('jboss.msc:type=container,name=jboss-as')
+  @msc.getServiceStatus(service_name).get('stateName')
+end
+
+def set_msc_service_mode(service_name, mode)
+  @msc ||= mbean('jboss.msc:type=container,name=jboss-as')
+  @msc.setServiceMode(service_name, mode)
+end
+
+def verify_msc_service_state(service_name, state, options={})
+  options[:timeout] ||= 30 # default to wait 30 seconds
+  service_state = nil
+  elapsed_seconds = 0
+  until service_state == state || elapsed_seconds > options[:timeout] do
+    service_state = get_msc_service_state(service_name)
+    sleep(0.5)
+  end
+  service_state.should == state
+end
