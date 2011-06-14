fe55c85a7d82158b1cda773c28320862442f214e
Jim Crossley <jim@crossleys.org>
2011-06-14 10:44:56 -0400

    Fix potential DRb service startup race condition resulting in a connection refused error during the test.

--- integration-tests/spec/spec_helper.rb
+++ integration-tests/spec/spec_helper.rb
@@ -5,12 +5,13 @@ TorqueSpec.local {
   require 'spec_helper_integ'
 }
 
-TorqueSpec.jboss_home = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'integ-dist', 'jboss' ) )
-TorqueSpec.max_heap = java.lang::System.getProperty( 'max.heap' )
-TorqueSpec.lazy = java.lang::System.getProperty( 'jboss.lazy' ) == "true"
-TorqueSpec.jvm_args += " -Djruby.home=#{java.lang::System.getProperty( 'jruby.home' )}"
-
-TorqueSpec.knob_root = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'knobs' ) )
+TorqueSpec.configure do |config|
+  config.jboss_home = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'integ-dist', 'jboss' ) )
+  config.max_heap = java.lang::System.getProperty( 'max.heap' )
+  config.lazy = java.lang::System.getProperty( 'jboss.lazy' ) == "true"
+  config.jvm_args += " -Djruby.home=#{java.lang::System.getProperty( 'jruby.home' )}"
+  config.knob_root = File.expand_path( File.join( File.dirname( __FILE__ ), '..', 'target', 'knobs' ) )
+end
 FileUtils.mkdir_p(TorqueSpec.knob_root) unless File.exist?(TorqueSpec.knob_root)
 
 MUTABLE_APP_BASE_PATH  = File.join( File.dirname( __FILE__ ), '..', 'target', 'apps' )
