908fc60297f6bf8de1c0a538d94201c1a325694a
Jim Crossley <jim@crossleys.org>
2011-06-21 21:11:32 -0400

    Revert "Less? Sure. Better? Maybe."
    This reverts commit f721868e085948d18eb62bb6df4098e5e9a8bab6.

--- integration-tests/spec/basic_bundler_spec.rb
+++ integration-tests/spec/basic_bundler_spec.rb
@@ -1,16 +1,21 @@
 require 'spec_helper'
 
-remote_describe "basic bundler test with rack" do
+describe "basic bundler test with rack" do
 
   deploy <<-END.gsub(/^ {4}/,'')
+
     application:
       root: #{File.dirname(__FILE__)}/../apps/rack/basic_bundler
+      env: development
+    web:
+      context: /basic-bundler
     ruby:
       version: #{RUBY_VERSION[0,3]}
+
   END
 
   it "should work with an old version of rack gem" do
-    require 'bundler/setup'
+    visit "/basic-bundler"
+    page.should have_content('it worked')
   end
-
 end
