6d62240277fad5d456ce4629828fa134076a57d6
Ben Browning <bbrownin@redhat.com>
2011-06-27 10:57:06 -0400

    Port messaging to the new controller style

--- modules/messaging/src/main/java/org/torquebox/messaging/as/MessagingExtension.java
+++ modules/messaging/src/main/java/org/torquebox/messaging/as/MessagingExtension.java
@@ -19,12 +19,12 @@
 
 package org.torquebox.messaging.as;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 
 import org.jboss.as.controller.ExtensionContext;
 import org.jboss.as.controller.SubsystemRegistration;
 import org.jboss.as.controller.parsing.ExtensionParsingContext;
-import org.jboss.as.controller.registry.ModelNodeRegistration;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.logging.Logger;
 import org.torquebox.core.as.AbstractBootstrappableExtension;
 
@@ -35,7 +35,7 @@ public class MessagingExtension extends AbstractBootstrappableExtension {
         bootstrap();
         log.info( "Initializing TorqueBox Messaging Subsystem" );
         final SubsystemRegistration registration = context.registerSubsystem( SUBSYSTEM_NAME );
-        final ModelNodeRegistration subsystem = registration.registerSubsystemModel( MessagingSubsystemProviders.SUBSYSTEM );
+        final ManagementResourceRegistration subsystem = registration.registerSubsystemModel( MessagingSubsystemProviders.SUBSYSTEM );
 
         subsystem.registerOperationHandler( ADD,
                 MessagingSubsystemAdd.ADD_INSTANCE,
--- modules/messaging/src/main/java/org/torquebox/messaging/as/MessagingSubsystemAdd.java
+++ modules/messaging/src/main/java/org/torquebox/messaging/as/MessagingSubsystemAdd.java
@@ -19,25 +19,26 @@
 
 package org.torquebox.messaging.as;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+import java.util.List;
 
 import javax.jms.ConnectionFactory;
 
-import org.jboss.as.controller.BasicOperationResult;
-import org.jboss.as.controller.ModelAddOperationHandler;
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.controller.OperationResult;
-import org.jboss.as.controller.ResultHandler;
-import org.jboss.as.controller.RuntimeTask;
-import org.jboss.as.controller.RuntimeTaskContext;
+import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.naming.ManagedReferenceFactory;
 import org.jboss.as.naming.deployment.ContextNames;
-import org.jboss.as.server.BootOperationContext;
-import org.jboss.as.server.BootOperationHandler;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
 import org.jboss.as.server.deployment.Phase;
 import org.jboss.dmr.ModelNode;
 import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceController.Mode;
 import org.jboss.msc.service.ServiceName;
 import org.torquebox.core.injection.jndi.ManagedReferenceInjectableService;
@@ -57,99 +58,79 @@ import org.torquebox.messaging.TopicsYamlParsingProcessor;
 import org.torquebox.messaging.component.MessageProcessorComponentResolverInstaller;
 import org.torquebox.messaging.injection.RubyConnectionFactoryService;
 
-class MessagingSubsystemAdd implements ModelAddOperationHandler, BootOperationHandler {
-
-    /** {@inheritDoc} */
+class MessagingSubsystemAdd extends AbstractBoottimeAddStepHandler {
+    
     @Override
-    public OperationResult execute(final OperationContext context, final ModelNode operation, final ResultHandler resultHandler) {
-        final ModelNode subModel = context.getSubModel();
-        subModel.setEmptyObject();
-
-        if (context instanceof BootOperationContext) {
-            handleBootContext( context, resultHandler );
-        } else {
-            resultHandler.handleResultComplete();
-        }
-        return compensatingResult( operation );
+    protected void populateModel(ModelNode operation, ModelNode model) {
+        model.setEmptyObject();
     }
-
-    protected boolean handleBootContext(final OperationContext operationContext, final ResultHandler resultHandler) {
-
-        if (!(operationContext instanceof BootOperationContext)) {
-            return false;
-        }
-
-        final BootOperationContext context = (BootOperationContext) operationContext;
-
-        context.getRuntimeContext().setRuntimeTask( bootTask( context, resultHandler ) );
-        return true;
-    }
-
-    protected void addDeploymentProcessors(final BootOperationContext context) {
-
-    	context.addDeploymentProcessor( Phase.PARSE, 10, new BackgroundablePresetsDeployer() );
-        context.addDeploymentProcessor( Phase.PARSE, 11, new QueuesYamlParsingProcessor() );
-        context.addDeploymentProcessor( Phase.PARSE, 12, new TopicsYamlParsingProcessor() );
-        context.addDeploymentProcessor( Phase.PARSE, 13, new MessagingYamlParsingProcessor() );
-        context.addDeploymentProcessor( Phase.PARSE, 40, new TasksYamlParsingProcessor() );
-        context.addDeploymentProcessor( Phase.PARSE, 41, new TasksScanningDeployer() );
-
-        context.addDeploymentProcessor( Phase.DEPENDENCIES, 3, new MessagingDependenciesProcessor() );
+    
+    @Override
+    protected void performBoottime(OperationContext context, ModelNode operation, ModelNode model,
+                                   ServiceVerificationHandler verificationHandler,
+                                   List<ServiceController<?>> newControllers) throws OperationFailedException {
         
-        context.addDeploymentProcessor( Phase.CONFIGURE_MODULE, 0, new MessagingLoadPathProcessor() );
+        context.addStep( new AbstractDeploymentChainStep() {
+            @Override
+            protected void execute(DeploymentProcessorTarget processorTarget) {
+                addDeploymentProcessors( processorTarget );
+            }
+        }, OperationContext.Stage.RUNTIME );
+        
+        addMessagingServices( context, verificationHandler, newControllers );
+    }
+
+    protected void addDeploymentProcessors(final DeploymentProcessorTarget processorTarget) {
+
+    	processorTarget.addDeploymentProcessor( Phase.PARSE, 10, new BackgroundablePresetsDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.PARSE, 11, new QueuesYamlParsingProcessor() );
+        processorTarget.addDeploymentProcessor( Phase.PARSE, 12, new TopicsYamlParsingProcessor() );
+        processorTarget.addDeploymentProcessor( Phase.PARSE, 13, new MessagingYamlParsingProcessor() );
+        processorTarget.addDeploymentProcessor( Phase.PARSE, 40, new TasksYamlParsingProcessor() );
+        processorTarget.addDeploymentProcessor( Phase.PARSE, 41, new TasksScanningDeployer() );
+
+        processorTarget.addDeploymentProcessor( Phase.DEPENDENCIES, 3, new MessagingDependenciesProcessor() );
+        
+        processorTarget.addDeploymentProcessor( Phase.CONFIGURE_MODULE, 0, new MessagingLoadPathProcessor() );
 
         //context.addDeploymentProcessor( Phase.POST_MODULE, 7, new MessagingInjectablesProcessor() );
-        context.addDeploymentProcessor( Phase.POST_MODULE, 11, new ApplicationNamingContextBindingProcessor() );
+        processorTarget.addDeploymentProcessor( Phase.POST_MODULE, 11, new ApplicationNamingContextBindingProcessor() );
         
-        context.addDeploymentProcessor( Phase.POST_MODULE, 220, new TasksDeployer() );
-        context.addDeploymentProcessor( Phase.POST_MODULE, 320, new MessagingRuntimePoolDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.POST_MODULE, 220, new TasksDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.POST_MODULE, 320, new MessagingRuntimePoolDeployer() );
 
-        context.addDeploymentProcessor( Phase.INSTALL, 120, new MessageProcessorComponentResolverInstaller() );
-        context.addDeploymentProcessor( Phase.INSTALL, 220, new MessageProcessorDeployer() );
-        context.addDeploymentProcessor( Phase.INSTALL, 221, new QueueDeployer() );
-        context.addDeploymentProcessor( Phase.INSTALL, 222, new TopicDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.INSTALL, 120, new MessageProcessorComponentResolverInstaller() );
+        processorTarget.addDeploymentProcessor( Phase.INSTALL, 220, new MessageProcessorDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.INSTALL, 221, new QueueDeployer() );
+        processorTarget.addDeploymentProcessor( Phase.INSTALL, 222, new TopicDeployer() );
     }
 
-    protected void addMessagingServices(final RuntimeTaskContext context) {
-        addRubyConnectionFactory( context );
+    protected void addMessagingServices(final OperationContext context, ServiceVerificationHandler verificationHandler,
+                                        List<ServiceController<?>> newControllers) {
+        addRubyConnectionFactory( context, verificationHandler, newControllers );
     }
 
-    protected void addRubyConnectionFactory(final RuntimeTaskContext context) {
+    protected void addRubyConnectionFactory(final OperationContext context, ServiceVerificationHandler verificationHandler,
+                                            List<ServiceController<?>> newControllers) {
 
         ServiceName managedFactoryServiceName = MessagingServices.RUBY_CONNECTION_FACTORY.append( "manager" );
 
         ManagedReferenceInjectableService managementService = new ManagedReferenceInjectableService();
-        context.getServiceTarget().addService( managedFactoryServiceName, managementService )
+        newControllers.add( context.getServiceTarget().addService( managedFactoryServiceName, managementService )
                 .addDependency( getJMSConnectionFactoryServiceName(), ManagedReferenceFactory.class, managementService.getManagedReferenceFactoryInjector() )
-                .install();
+                .addListener( verificationHandler )
+                .install() );
 
         RubyConnectionFactoryService service = new RubyConnectionFactoryService();
-        context.getServiceTarget().addService( MessagingServices.RUBY_CONNECTION_FACTORY, service )
+        newControllers.add( context.getServiceTarget().addService( MessagingServices.RUBY_CONNECTION_FACTORY, service )
                 .addDependency( managedFactoryServiceName, ConnectionFactory.class, service.getConnectionFactoryInjector() )
+                .addListener( verificationHandler )
                 .setInitialMode( Mode.ON_DEMAND )
-                .install();
+                .install() );
     }
     
     protected ServiceName getJMSConnectionFactoryServiceName() {
         return ContextNames.JAVA_CONTEXT_SERVICE_NAME.append( "ConnectionFactory" );
-    }
-
-    protected RuntimeTask bootTask(final BootOperationContext bootContext, final ResultHandler resultHandler) {
-        return new RuntimeTask() {
-            @Override
-            public void execute(RuntimeTaskContext context) throws OperationFailedException {
-                addDeploymentProcessors( bootContext );
-                addMessagingServices( context );
-                resultHandler.handleResultComplete();
-            }
-        };
-    }
-
-    protected BasicOperationResult compensatingResult(ModelNode operation) {
-        final ModelNode compensatingOperation = new ModelNode();
-        compensatingOperation.get( OP ).set( REMOVE );
-        compensatingOperation.get( OP_ADDR ).set( operation.get( OP_ADDR ) );
-        return new BasicOperationResult( compensatingOperation );
     }
 
     static ModelNode createOperation(ModelNode address) {
