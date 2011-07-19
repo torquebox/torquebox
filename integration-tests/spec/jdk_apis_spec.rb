bc15d73d24a63667dbe580a1d1e8846dd06cb357
Ben Browning <bbrownin@redhat.com>
2011-07-19 15:47:32 -0400

    All the JDK APIs are made available via the "system" module (TORQUE-500)

---
+++ integration-tests/spec/jdk_apis_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+remote_describe 'jdk apis' do
+
+  it "should be exposed" do
+    lambda {
+      javax.ejb.SessionBean
+      javax.faces.FactoryFinder
+      javax.jms.Destination
+      javax.mail.Address
+      javax.servlet.Filter
+      javax.transaction.Transaction
+      javax.xml.stream.StreamFilter
+    }.should_not raise_error
+  end
+end
