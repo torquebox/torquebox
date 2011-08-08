f25c72ef9312582c43d3c7c9dc2037c50dbb4d01
Tobias Crawley <tcrawley@gmail.com>
2011-08-08 08:13:15 -0400

    A fresh rails 3.1.rc5 app.

---
+++ integration-tests/apps/rails3.1/basic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basic::Application.config.secret_token = '224a5a743c7b6b259eedbd34a279b76d7dfca95d7bd68c0bb8dab0b4dc4cc77b131fbd291bec55c88a700bbe8a993102961f2251a55ebf9806cbe566147c72d1'
