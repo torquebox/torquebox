cf9f2b52669fcd930cf2fdfc5ac0aeee6c1a774a
Tobias Crawley <tcrawley@gmail.com>
2011-08-08 07:57:02 -0400

    Remove existing rails 3.1 basic integ app so we can regenerate it cleanly.

--- integration-tests/apps/rails3.1/basic/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  include TorqueBox::Injectors
-  
-  protect_from_forgery
-
-  def thing_one
-      inject( org.torquebox.ThingOne )
-  end
-end
