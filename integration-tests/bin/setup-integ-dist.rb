94f8191c701ab0964739c123f61023984f7fee7e
Bob McWhirter <bob@mcwhirter.org>
2011-08-10 17:12:00 -0400

    Fix integ setup for both unix and windows.  mkdir -p ftw.

--- integration-tests/bin/setup-integ-dist.rb
+++ integration-tests/bin/setup-integ-dist.rb
@@ -1,3 +1,4 @@
+
 require 'java'
 require 'fileutils'
 
@@ -7,35 +8,32 @@ output_dir   = File.expand_path( ARGV[1] )
 puts "assembly from... #{assembly_dir}"
 puts "output to....... #{output_dir}"
 
-if ( assembly_dir == output_dir )
-  puts "*** Setting up integration tests against assembly, NOT A COPY."
-else
-  Dir.chdir( assembly_dir ) do
-    if ( java.lang::System.getProperty( 'os.name' ) =~ /.*windows.*/i )
-      puts "*** Syncing to integ-dist by copy"
-      if ( File.exists?( output_dir ) )
-        puts "*** Only copying torquebox modules"
-        modules_path = 'jboss/modules/org/torquebox' 
-        FileUtils.rm_rf( "#{output_dir}/#{modules_path}" )
-        FileUtils.cp_r( "./#{modules_path}", "#{output_dir}/#{modules_path}" )
-      else
-        FileUtils.mkdir_p( File.dirname( output_dir ) )
-        FileUtils.rm_rf( output_dir )
-        FileUtils.cp_r( '.', output_dir )
-      end
+Dir.chdir( assembly_dir ) do
+  if ( java.lang::System.getProperty( 'os.name' ) =~ /.*windows.*/i )
+    puts "*** Syncing to integ-dist by copy"
+    if ( File.exists?( output_dir ) )
+      puts "*** Only copying torquebox modules"
+      modules_path = 'jboss/modules/org/torquebox'
+      FileUtils.rm_rf( "#{output_dir}/#{modules_path}" )
+      FileUtils.mkdir_p( File.dirname( "#{output_dir}/#{modules_path}" ) )
+      FileUtils.cp_r( "./#{modules_path}", "#{output_dir}/#{modules_path}" )
     else
-      puts "*** Syncing to integ-dist by rsync"
-      cmd = [ 'rsync -av . --relative',
-              '--include jboss/modules',
-              '--include jboss/standalone',
-              '--exclude jruby/share/ri',
-              '--exclude jruby/lib/ruby/gems/1.8/doc',
-              output_dir ].join( ' ' )
-      puts cmd
-      puts `#{cmd}` 
+      FileUtils.mkdir_p( File.dirname( output_dir ) )
+      FileUtils.rm_rf( output_dir )
+      FileUtils.cp_r( '.', output_dir )
     end
+  else
+    puts "*** Syncing to integ-dist by rsync"
+    FileUtils.mkdir_p( File.dirname( output_dir ) )
+    cmd = [ 'rsync -av . --relative',
+            '--include jboss/modules',
+            '--include jboss/standalone',
+            '--exclude jruby/share/ri',
+            '--exclude jruby/lib/ruby/gems/1.8/doc',
+            output_dir ].join( ' ' )
+    puts cmd
+    puts `#{cmd}`
   end
 end
 
 FileUtils.rm_rf( Dir["#{output_dir}/jboss/standalone/log/*"] )
-
