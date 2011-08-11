3bb8c65bf4783ac70b140f2787278b897d658fba
Jim Crossley <jim@crossleys.org>
2011-08-11 11:38:50 -0400

    Virgin rails 3.0.9 app for testing transaction expectations

---
+++ integration-tests/apps/rails3/transactions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transactions::Application.config.secret_token = '3ea9e7b8a67727b3d00c75bd9a29d8927396b56af8aa0ddc7051e5b2e03855efbdd5e70f0ca7392506cc6a42c7bb792807e99a9e8d0e2bcc35d1efb5607ad275'
