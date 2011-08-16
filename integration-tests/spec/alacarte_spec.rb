7934182f74450e817f391ab7a248628d4f779df4
Tobias Crawley <tcrawley@gmail.com>
2011-08-16 09:19:22 -0400

    Bump up the timeout.

--- integration-tests/spec/alacarte_spec.rb
+++ integration-tests/spec/alacarte_spec.rb
@@ -1,14 +1,14 @@
-require 'spec_helper'
+ingrequire 'spec_helper'
 require 'torquebox-messaging'
 
 shared_examples_for "alacarte" do
 
-  it "should detect file writing activity" do
+  it "should detect activity" do
     responseq = TorqueBox::Messaging::Queue.new( '/queue/response' )
     response = responseq.receive( :timeout => 120_000 )
     5.times do
       response.should == 'done'
-      response = responseq.receive( :timeout => 2_000 )
+      response = responseq.receive( :timeout => 10_000 )
     end
   end
 
