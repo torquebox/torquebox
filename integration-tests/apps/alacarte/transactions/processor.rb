015f39f377a503fa4c3a64f95af5b017a0ea5161
Jim Crossley <jim@crossleys.org>
2011-08-16 20:56:00 -0400

    Test for message delivery retries, since my untransacted session machinations broke them, unbeknownst to anyone.

--- integration-tests/apps/alacarte/transactions/processor.rb
+++ integration-tests/apps/alacarte/transactions/processor.rb
@@ -6,9 +6,22 @@ class Processor < TorqueBox::Messaging::MessageProcessor
 
   def on_message(msg)
     output = inject( '/queue/output' )
-    output.publish('yay!')
     raise msg if msg =~ /error/
+    if msg =~ /\s+(\d)\s+retries/
+      output.publish(test_retries($1.to_i))
+    else
+      output.publish('yay!')
+    end
   end
 
+  def test_retries(count)
+    if $count == count
+      "retried successfully #{count} times"
+    else
+      $count ||= 0
+      $count += 1
+      raise "retry count=#{$count}"
+    end
+  end
 end
 
--- integration-tests/spec/transaction_spec.rb
+++ integration-tests/spec/transaction_spec.rb
@@ -22,6 +22,7 @@ remote_describe "transactions testing" do
     input.publish("anything")
     thread.join
     response.should be_nil
+    output.receive.should == 'yay!' # drain the queue for next test
   end
 
   it "should receive a message when no error occurs" do
@@ -34,6 +35,14 @@ remote_describe "transactions testing" do
     input.publish("anything")
     thread.join
     response.should_not be_nil
+  end
+
+  it "should retry delivery when an error is tossed" do
+    input = inject('/queue/input')
+    output = inject('/queue/output')
+    input.publish("This message should trigger 5 retries")
+    response = output.receive(:timeout => 10_000)
+    response.should match /success.*\s5\s/
   end
 
   it "should not receive a message when an error is tossed" do
