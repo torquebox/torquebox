efd23256c5e38587e9e394f480c1b80d55789104
Tobias Crawley <tcrawley@gmail.com>
2011-08-23 15:43:01 -0400

    Let's try a binary file as well.

--- integration-tests/spec/form_handling_spec.rb
+++ integration-tests/spec/form_handling_spec.rb
@@ -51,7 +51,28 @@ describe "rails3 form handling" do
   end
 
 
-  it "should be able to write the file" do
+  it "should be able to write a binary file" do
+    visit "/basic-rails/form_handling/upload_file"
+    page.driver.cookies.count.should == 1
+    session_id = page.driver.cookies["JSESSIONID"].value
+    session_id.length.should be > 0
+    auth_token = find(:xpath, "//input[@name='authenticity_token']").value
+    auth_token.length.should be > 0
+    find("#the-upload-form")["action"].should include(session_id)
+
+    data_path = File.expand_path( File.join( File.dirname( __FILE__ ), "..", "target/test-classes/rails.png" ) ) 
+    attach_file("the-file", data_path )
+    
+    page.driver.cookies.clear
+    click_button("Save changes")
+
+    saved_path = find("#path").text
+
+    File.size( saved_path ).should == File.size( data_path )
+  end
+
+  
+  it "should be able to write a multiline text file" do
     visit "/basic-rails/form_handling/upload_file"
     page.driver.cookies.count.should == 1
     session_id = page.driver.cookies["JSESSIONID"].value
@@ -68,10 +89,7 @@ describe "rails3 form handling" do
 
     saved_path = find("#path").text
 
-    data_contents = File.read( data_path )
-    saved_contents = File.read( saved_path )
-    
-    saved_contents.should == data_contents
+    File.size( saved_path ).should == File.size( data_path )
   end
 
 end
