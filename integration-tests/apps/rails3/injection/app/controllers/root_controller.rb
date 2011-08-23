2d5ce57c705ff9335dd3b16c74f0d8a80a6eb907
Tobias Crawley <tcrawley@gmail.com>
2011-08-23 10:49:06 -0400

    Pump up the timeout, pump up the timeout, pump it up.

--- integration-tests/apps/rails3/injection/app/controllers/root_controller.rb
+++ integration-tests/apps/rails3/injection/app/controllers/root_controller.rb
@@ -3,33 +3,33 @@ class RootController < ApplicationController
 
   def service
     queue = inject('/queues/injection_service')
-    @message = queue.receive(:timeout => 60000)
+    @message = queue.receive(:timeout => 120_000)
     render 'injection.html.erb'
   end
 
   def job
     queue = inject('/queues/injection_job')
-    @message = queue.receive(:timeout => 60000)
+    @message = queue.receive(:timeout => 120_000)
     render :injection
   end
 
   def task
     InjectionTask.async(:publish_message)
     queue = inject('/queues/injection_task')
-    @message = queue.receive(:timeout => 60000)
+    @message = queue.receive(:timeout => 120_000)
     render 'injection.html.erb'
   end
 
   def alt_inject
     queue = __inject__('/queues/injection_job')
-    @message = queue.receive(:timeout => 60000)
+    @message = queue.receive(:timeout => 120_000)
     render :injection
   end
 
   def enumerable
     EnumerableThing.new
     queue = inject('/queues/injection_enumerable')
-    @message = queue.receive(:timeout => 60000)
+    @message = queue.receive(:timeout => 120_000)
     render :injection
   end
   
