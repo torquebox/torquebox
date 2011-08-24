b81613965717070b19da05302e2ae69377feca63
Tobias Crawley <tcrawley@gmail.com>
2011-08-24 14:01:09 -0400

    Revert "Test for file writing."
    This reverts commit 43574036f2c258760233d53e631401bcb77842aa.

--- integration-tests/apps/rails3/basic/app/controllers/form_handling_controller.rb
+++ integration-tests/apps/rails3/basic/app/controllers/form_handling_controller.rb
@@ -10,10 +10,7 @@ class FormHandlingController < ApplicationController
 
   def upload_file
     if ( request.put? )
-      file_contents = params[:upload_file].read
-      @data = "#{file_contents} As returned."
-      @path = File.join( Dir.tmpdir, "file_#{Time.now.to_f}" )
-      File.open( @path, 'wb' ) { |f| f.write( file_contents ) }
+      @data = "#{params[:upload_file].read} As returned."
     end
   end
 
