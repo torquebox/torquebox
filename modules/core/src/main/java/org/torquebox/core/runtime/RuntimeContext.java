61f053fba254d0cb430881ae9240e87babc788cf
Bob McWhirter <bob@mcwhirter.org>
2011-09-07 21:55:16 -0400

    Yo dawg, I herd you liked threads and heap, so I put a thread in your thread in your thread, so you can spawn while you spawn without exhausting your heap.
    DRY up all the context-setting around the multitude of methods for invoking
    Ruby, using a Callable<?>.
    
    Associate RuntimeContext's Ruby based on the JRubyClassLoader which we
    jam into the TCCL.

--- modules/core/src/main/java/org/torquebox/core/runtime/RuntimeContext.java
+++ modules/core/src/main/java/org/torquebox/core/runtime/RuntimeContext.java
@@ -1,16 +1,31 @@
 package org.torquebox.core.runtime;
 
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import org.jruby.Ruby;
+import org.jruby.util.JRubyClassLoader;
 
 public class RuntimeContext {
-    
+
     public static Ruby getCurrentRuntime() {
-       return RuntimeContext.runtime.get(); 
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        while (cl != null) {
+            if (cl instanceof JRubyClassLoader) {
+                WeakReference<Ruby> ref = contexts.get( cl );
+                return ref.get();
+            }
+            cl = cl.getParent();
+        }
+
+        return null;
     }
 
-    public static void setCurrentRuntime(Ruby ruby) {
-        RuntimeContext.runtime.set( ruby );
+    public static void registerRuntime(Ruby ruby) {
+        contexts.put( ruby.getJRubyClassLoader(), new WeakReference<Ruby>(ruby) );
+        return;
     }
-    
-    private static final ThreadLocal<Ruby> runtime = new ThreadLocal<Ruby>();
+
+    private static final Map<JRubyClassLoader, WeakReference<Ruby>> contexts = new WeakHashMap<JRubyClassLoader, WeakReference<Ruby>>();
 }
