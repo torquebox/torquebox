a6ed1efba51986966978eb253fb2af49c0b571af
Bob McWhirter <bob@mcwhirter.org>
2011-09-07 21:55:16 -0400

    Yo dawg, I herd you liked threads, so I put a thread in your thread in your thread, so you can spawn while you spawn.
    DRY up all the context-setting around the multitude of methods for invoking
    Ruby, using a Callable<?>.
    
    Associate RuntimeContext's Ruby based on the JRubyClassLoader which we
    jam into the TCCL.

--- modules/core/src/main/java/org/torquebox/core/runtime/RuntimeContext.java
+++ modules/core/src/main/java/org/torquebox/core/runtime/RuntimeContext.java
@@ -1,16 +1,30 @@
 package org.torquebox.core.runtime;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import org.jruby.Ruby;
 
 public class RuntimeContext {
-    
+
     public static Ruby getCurrentRuntime() {
-       return RuntimeContext.runtime.get(); 
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        if (cl == null) {
+            return null;
+        }
+        System.err.println( "getting for CL: " + cl );
+        return contexts.get( cl );
     }
 
     public static void setCurrentRuntime(Ruby ruby) {
-        RuntimeContext.runtime.set( ruby );
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        System.err.println( "setting for CL: " + cl );
+        System.err.println( " -->" + ruby );
+        if (cl == null) {
+            return;
+        }
+        contexts.put(  cl, ruby );
     }
-    
-    private static final ThreadLocal<Ruby> runtime = new ThreadLocal<Ruby>();
+
+    private static final Map<ClassLoader, Ruby> contexts = new WeakHashMap<ClassLoader, Ruby>();
 }
