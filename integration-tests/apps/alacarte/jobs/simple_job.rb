2391502f5efbd84eec0026c7e04724cf0fe49d76
Bob McWhirter <bob@mcwhirter.org>
2011-09-12 11:28:21 -0400

    Test for TORQUE-539.  I think this was inadvertantly broken and then inadvertantly fixed again.

--- integration-tests/apps/alacarte/jobs/simple_job.rb
+++ integration-tests/apps/alacarte/jobs/simple_job.rb
@@ -4,14 +4,15 @@ class SimpleJob
 
   def initialize(opts)
     @options = opts
+    @polish     = inject( Java::pl.softwaremine.ThingThree )
     @response_queue = inject( '/queue/response' )
     @init_params_queue = inject( '/queue/init_params' )
     @init_params_queue.publish( @options )
   end
 
   def run()
-    $stderr.puts "Job executing! queue is #{@response_queue}"
-    @response_queue.publish( 'done' )
+    $stderr.puts "Job executing! queue is #{@response_queue} and polish is #{@polish}"
+    @response_queue.publish( 'done' ) if @polish
   end
 
 end
