d6bcea1dd03d9b164eb5b55efd44122efe2d1255
Jim Crossley <jim@crossleys.org>
2011-09-20 16:32:34 -0400

    Migrate during deploy

--- integration-tests/apps/rails3.1/transactions/config/application.rb
+++ integration-tests/apps/rails3.1/transactions/config/application.rb
@@ -44,5 +44,14 @@ module Transactions
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.after_initialize do
+      begin
+        ActiveRecord::Migrator.migrate(RAILS_ROOT + "/db/migrate")
+      rescue Exception
+        puts "Ignoring migration error, probably due to multiple runtimes initializing: #{$!}"
+      end
+    end
+
   end
 end
