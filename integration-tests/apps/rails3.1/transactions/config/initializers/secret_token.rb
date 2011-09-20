12dde64aec05d6e5e49356c1e23b2fdba1b8b0f7
Jim Crossley <jim@crossleys.org>
2011-09-20 15:17:45 -0400

    Base app for Rails 3.1 tx tests

---
+++ integration-tests/apps/rails3.1/transactions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transactions::Application.config.secret_token = '1bc93aaff9b556d85eb4357a26087b2c02e1f8488c206742da7745a008d671d09a8d2066896f8884ce24a753275a8dd9cce51a2dfa0b8643e4c0640bf9a5e640'
