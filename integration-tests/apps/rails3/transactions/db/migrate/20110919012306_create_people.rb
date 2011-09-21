8a4c49e2f068ff8e4d892ed380fb4ab37eb4d046
Jim Crossley <jim@crossleys.org>
2011-09-21 11:55:55 -0400

    BAM! Multiple class-specific databases can participate in a single XA transaction!
    Unfortunately, the hackiness I'm doing in the migration doesn't seem
    to work on Rails 3.1, but it may be an issue with ar-jdbc 1.2.0, too.

--- integration-tests/apps/rails3/transactions/db/migrate/20110919012306_create_people.rb
+++ integration-tests/apps/rails3/transactions/db/migrate/20110919012306_create_people.rb
@@ -1,13 +1,13 @@
 class CreatePeople < ActiveRecord::Migration
   def self.up
-    # ActiveRecord::Base.establish_connection :person_database
+    ActiveRecord::Base.establish_connection :person_database
     create_table :people do |t|
       t.string :name
       t.integer :age
 
       t.timestamps
     end
-    # ActiveRecord::Base.establish_connection Rails.env
+    ActiveRecord::Base.establish_connection Rails.env
   end
 
   def self.down
