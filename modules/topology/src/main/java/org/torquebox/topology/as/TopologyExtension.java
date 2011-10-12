53fda1b4e03ab94190850716d55c4279aa5207c3
Bob McWhirter <bob@mcwhirter.org>
2011-10-12 18:00:33 -0400

    Stub in a topology module to hold cluster-topology machinations to ultimately support HA (singleton) services and jobs.

---
+++ modules/topology/src/main/java/org/torquebox/topology/as/TopologyExtension.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2008-2011 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.torquebox.topology.as;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+
+import org.jboss.as.controller.ExtensionContext;
+import org.jboss.as.controller.SubsystemRegistration;
+import org.jboss.as.controller.parsing.ExtensionParsingContext;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.logging.Logger;
+import org.projectodd.polyglot.core.as.AbstractBootstrappableExtension;
+
+public class TopologyExtension extends AbstractBootstrappableExtension {
+
+    @Override
+    public void initialize(ExtensionContext context) {
+        bootstrap();
+        log.info( "Initializing TorqueBox Topology Subsystem" );
+        final SubsystemRegistration registration = context.registerSubsystem( SUBSYSTEM_NAME );
+        final ManagementResourceRegistration subsystem = registration.registerSubsystemModel( TopologySubsystemProviders.SUBSYSTEM );
+
+        subsystem.registerOperationHandler( ADD,
+                TopologySubsystemAdd.ADD_INSTANCE,
+                TopologySubsystemProviders.SUBSYSTEM_ADD,
+                false );
+        
+        registration.registerXMLElementWriter(TopologySubsystemParser.getInstance());
+    }
+
+    @Override
+    public void initializeParsers(ExtensionParsingContext context) {
+        context.setSubsystemXmlMapping(Namespace.CURRENT.getUriString(), TopologySubsystemParser.getInstance());
+    }
+    
+    public static final String SUBSYSTEM_NAME = "torquebox-topology";
+    static final Logger log = Logger.getLogger( "org.torquebox.topology.as" );
+
+}
---
+++ modules/topology/src/main/java/org/torquebox/topology/as/TopologySubsystemAdd.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2008-2011 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.torquebox.topology.as;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+import java.util.List;
+
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
+import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceController;
+
+public class TopologySubsystemAdd extends AbstractBoottimeAddStepHandler {
+    
+
+    @Override
+    protected void populateModel(ModelNode operation, ModelNode subModel) {
+    }
+
+    @Override
+    protected void performBoottime(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler,
+            List<ServiceController<?>> newControllers) throws OperationFailedException {
+
+        context.addStep( new AbstractDeploymentChainStep() {
+            @Override
+            protected void execute(DeploymentProcessorTarget processorTarget) {
+                addDeploymentProcessors( processorTarget );
+            }
+        }, OperationContext.Stage.RUNTIME );
+
+        try {
+            addCoreServices( context, operation, model, verificationHandler, newControllers );
+        } catch (Exception e) {
+            throw new OperationFailedException( e, null );
+        }
+    }
+    
+
+    protected void addCoreServices(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) {
+    }
+
+
+    protected void addDeploymentProcessors(final DeploymentProcessorTarget processorTarget) {
+        //processorTarget.addDeploymentProcessor( Phase.PARSE, 31, new StompYamlParsingProcessor() );
+    }
+
+    static ModelNode createOperation(ModelNode address) {
+        final ModelNode subsystem = new ModelNode();
+        subsystem.get( OP ).set( ADD );
+        subsystem.get( OP_ADDR ).set( address );
+        return subsystem;
+    }
+
+    static final TopologySubsystemAdd ADD_INSTANCE = new TopologySubsystemAdd();
+    static final Logger log = Logger.getLogger( "org.torquebox.topology.as" );
+
+}
