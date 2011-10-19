4c27a9d7a8547f63cbaeaabef44dcfa9b60480e9
Bob McWhirter <bob@mcwhirter.org>
2011-10-19 18:50:10 -0400

    Unpend the WAR test, which was fixed as a side-effect of fixing our own vhost-management when run under domain mode.

--- integration-tests/spec/basic_war_spec.rb
+++ integration-tests/spec/basic_war_spec.rb
@@ -5,7 +5,7 @@ describe "basic war" do
   deploy File.join(File.dirname(__FILE__), "../target/test-classes/node-info.war")
 
   it "should work" do
-    pending "figuring out ClassCastException from Tomcat"
+    #pending "figuring out ClassCastException from Tomcat"
     visit "/node-info"
     page.should have_content( 'JBoss-Cloud node info' )
   end
