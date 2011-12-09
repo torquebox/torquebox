812a30b0f2355f512bddef32078c18a6b4858737
Mike Dobozy <dobozysaurus@gmail.com>
2011-12-09 11:38:36 -0500

    added torquebox thor spec and updated docs

---
+++ integration-tests/spec/torquebox_thor_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe "torquebox thor utility tests" do
+
+  describe "torquebox deploy" do
+    
+    it "should deploy a basic app" do
+      Dir.chdir( root_dir ) do
+        check_deployment "deploy"
+        check_undeployment "undeploy"
+      end
+    end
+        
+    it "should deploy an app with a name specified on the command line" do
+      Dir.chdir( root_dir ) do
+        check_deployment( "deploy --name=foobedoo", 'foobedoo' )
+        check_undeployment( "undeploy --name=foobedoo", 'foobedoo')
+      end
+    end
+    
+    it "should deploy an app with a context path specified on the command line" do
+      Dir.chdir( root_dir ) do
+        check_deployment 'deploy --context_path=/leftorium'
+        contents = File.read("#{TorqueBox::DeployUtils.deploy_dir}/basic-knob.yml")
+        contents.should include('context: /leftorium')
+        check_undeployment 'undeploy'
+      end
+    end
+    
+    it "should deploy an app with an environment specified on the command line" do
+      Dir.chdir( root_dir ) do
+        check_deployment 'deploy --env=production'
+        contents = File.read("#{TorqueBox::DeployUtils.deploy_dir}/basic-knob.yml")
+        contents.should include('production')
+        check_undeployment 'undeploy'
+      end
+    end
+
+    it "should deploy an app with a root specified on the command line" do
+      check_deployment "deploy #{root_dir}"
+      Dir.chdir( root_dir ) do
+        check_undeployment "undeploy"
+      end
+    end
+    
+  end
+  
+  private
+  
+  def check_deployment(tb_command, name = 'basic', suffix = '-knob.yml')
+    output = tb(tb_command)
+    puts "\nFor command #{tb_command}, deploy output is: "
+    puts output
+    output.should include("Deployed: #{name}#{suffix}")
+    output.should include("into: #{TorqueBox::DeployUtils.deploy_dir}")
+    File.exist?("#{TorqueBox::DeployUtils.deploy_dir}/#{name}#{suffix}").should == true
+    File.exist?("#{TorqueBox::DeployUtils.deploy_dir}/#{name}#{suffix}.dodeploy").should == true
+  end
+  
+  def check_undeployment(tb_command, name = 'basic', suffix = '-knob.yml')
+    output = tb(tb_command)
+    puts "\nFor command #{tb_command}, output is: "
+    puts output
+    output.should include("Undeployed: #{name}#{suffix}")
+    output.should include("from: #{TorqueBox::DeployUtils.deploy_dir}")
+      
+    # give the AS as many as five seconds to undeploy
+    5.times { 
+      break unless File.exist?("#{TorqueBox::DeployUtils.deploy_dir}/#{name}#{suffix}") 
+      puts "Waiting for undeployment..."
+      sleep 1
+    }
+      
+    File.exist?("#{TorqueBox::DeployUtils.deploy_dir}/#{name}#{suffix}").should == false
+    File.exist?("#{TorqueBox::DeployUtils.deploy_dir}/#{name}#{suffix}.dodeploy").should == false
+  end  
+  
+  def root_dir
+    File.join( File.dirname(__FILE__), '..', 'apps', 'rails3.1', 'basic' )
+  end
+  
+  def tb(cmd)
+    integ_jruby("-S torquebox #{cmd}")
+  end  
+
+end
