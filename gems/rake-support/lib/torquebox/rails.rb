4e2c3dc316f7d8c7119a6cc74a336fb15d21741a
Lance Ball <lball@redhat.com>
2011-12-12 18:06:38 -0500

    [TORQUE-644] Make bin/torquebox rails ROOT applicable to an existing Rails app.

--- gems/rake-support/lib/torquebox/rails.rb
+++ gems/rake-support/lib/torquebox/rails.rb
@@ -5,9 +5,18 @@ module TorqueBox
   class Rails
     def self.new_app
       # Assumes ARGV[0] already has the application name
-      ARGV << [ "-m", "#{ENV['TORQUEBOX_HOME']}/share/rails/template.rb" ]
+      ARGV << [ "-m", TorqueBox::Rails.template ]
       ARGV.flatten!
       ::Rails::Generators::AppGenerator.start
+    end
+
+    def self.apply_template( root )
+      generator = ::Rails::Generators::AppGenerator.new( [root], {}, :destination_root => root )
+      generator.apply TorqueBox::Rails.template 
+    end
+
+    def self.template
+      "#{ENV['TORQUEBOX_HOME']}/share/rails/template.rb"
     end
   end
 end
