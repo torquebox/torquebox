b6a898b89f30cd886977932c121873d68040b07a
Carl Hörberg <carl.hoerberg@gmail.com>
2011-12-23 13:23:23 +0100

    Don't needlessly raise errors because of client abort exceptions

--- modules/web/src/main/java/org/torquebox/web/rack/response_handler.rb
+++ modules/web/src/main/java/org/torquebox/web/rack/response_handler.rb
@@ -44,6 +44,9 @@ module TorqueBox
           else
             out.write( body.to_java_bytes )
           end
+        rescue NativeException => e
+          # Don't needlessly raise errors because of client abort exceptions
+          raise unless e.cause.toString =~ /(clientabortexception|broken pipe)/i
         ensure
           body.close if body && body.respond_to?( :close )
         end
