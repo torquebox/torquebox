c134797ba873313d1111ef92408ff1d1943bd646
Jim Crossley <jim@crossleys.org>
2012-01-09 17:44:07 -0500

    Integ tests for prioritized message publishing

--- integration-tests/spec/messaging_spec.rb
+++ integration-tests/spec/messaging_spec.rb
@@ -59,6 +59,33 @@ remote_describe "in-container messaging tests" do
     end
   end
 
+  describe "message priorities" do
+    it "should be fifo for same priority messages" do
+      with_queue("/queues/priorities") do |queue|
+        queue.publish "first"
+        queue.publish "second"
+        queue.publish "third"
+        queue.publish "fourth"
+        queue.receive.should == "first"
+        queue.receive.should == "second"
+        queue.receive.should == "third"
+        queue.receive.should == "fourth"
+      end
+    end
+    it "should send higher priority messages first" do
+      with_queue("/queues/priorities") do |queue|
+        queue.publish "first", :priority => :low
+        queue.publish "second", :priority => :normal
+        queue.publish "third", :priority => :high
+        queue.publish "fourth", :priority => :critical
+        queue.receive.should == "fourth"
+        queue.receive.should == "third"
+        queue.receive.should == "second"
+        queue.receive.should == "first"
+      end
+    end
+  end
+
   context "message selectors" do
     {
       'prop = true' => true,
