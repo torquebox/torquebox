dc9ff9517647387cced2317918c08cf9fdb81052
Lance Ball <lball@redhat.com>
2012-02-13 10:57:35 -0500

    [TORQUE-704] Make torquebox rails work with Rails 2.x

--- gems/rake-support/lib/torquebox/rails.rb
+++ gems/rake-support/lib/torquebox/rails.rb
@@ -16,34 +16,56 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
 
-if ( defined? Rails::VERSION )
-  if ( Rails::VERSION::MAJOR == 3 )
-    require 'rails/generators'
-    require 'rails/generators/rails/app/app_generator'
-  else
-    require 'rails_generator'
-    require 'rails_generator/generators/applications/app/app_generator'
-  end
-  module TorqueBox
-    class Rails
-      def self.new_app
-        # Assumes ARGV[0] already has the application name
-        ARGV << [ "-m", TorqueBox::Rails.template ]
-        ARGV.flatten!
+begin
+  require 'rails/version'
+rescue
+end
+
+module TorqueBox
+  class Rails
+    if ( defined? ::Rails::VERSION )
+      if ( ::Rails::VERSION::MAJOR == 3 )
+        require 'rails/generators'
+        require 'rails/generators/rails/app/app_generator'
+        @@generator = ::Rails::Generators::AppGenerator
+        @@rails_version = 3
+      else
+        require 'rails_generator'
+        require 'rails_generator/generators/applications/app/app_generator'
+        @@generator = ::AppGenerator
+        @@rails_version = 2
+      end
+    else
+      $stderr.puts "Rails not installed. Unable to load generators"
+    end
+
+
+    def self.new_app
+      # Assumes ARGV[0] already has the application name
+      ARGV << [ "-m", TorqueBox::Rails.template ]
+      ARGV.flatten!
+      if @@rails_version == 3
         ::Rails::Generators::AppGenerator.start
-      end
-
-      def self.apply_template( root )
-        generator = ::Rails::Generators::AppGenerator.new( [root], {}, :destination_root => root )
-        generator.apply TorqueBox::Rails.template 
-      end
-
-      def self.template
-        "#{ENV['TORQUEBOX_HOME']}/share/rails/template.rb"
+      else
+        require 'rails_generator/scripts/generate'
+        ::Rails::Generator::Base.use_application_sources!
+        ::Rails::Generator::Scripts::Generate.new.run(ARGV, :generator => 'app')
       end
     end
+
+    def self.apply_template( root )
+      if @@rails_version == 3
+        generator = ::Rails::Generators::AppGenerator.new( [root], {}, :destination_root => root )
+        generator.apply TorqueBox::Rails.template 
+      else
+        require 'rails_generator/generators/applications/app/template_runner'
+        ::Rails::TemplateRunner.new( TorqueBox::Rails.template )
+      end        
+    end
+
+    def self.template
+      "#{ENV['TORQUEBOX_HOME']}/share/rails/template.rb"
+    end
   end
-else
-  $stderr.puts "Rails not installed. Unable to load generators"
 end
 
