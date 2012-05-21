02748ad2db4902570471fc22607565163a8da465
Lance Ball <lball@redhat.com>
2012-05-21 09:14:02 -0400

    [TORQUE-830] Add a spec.

---
+++ integration-tests/apps/rails3/basic-with-explicit-cache/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Basic::Application.config.session_store :torquebox_store
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Basic::Application.config.session_store :active_record_store
