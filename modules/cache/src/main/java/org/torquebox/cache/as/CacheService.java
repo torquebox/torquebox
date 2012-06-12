5f142406e23a20177755f8811ac4116d7f4b1299
Lance Ball <lball@redhat.com>
2012-06-12 16:21:55 -0400

    Fix formatting per TorqueBox standards

--- modules/cache/src/main/java/org/torquebox/cache/as/CacheService.java
+++ modules/cache/src/main/java/org/torquebox/cache/as/CacheService.java
@@ -11,56 +11,57 @@ import org.jboss.msc.service.StopContext;
 
 public class CacheService implements Service<CacheService> {
 
-	private boolean clustered = false;
-	private CacheContainer container; 
+    private boolean clustered = false;
+    private CacheContainer container;
 
-	@Override
-	public CacheService getValue() throws IllegalStateException, IllegalArgumentException {
-		return this;
-	}
+    @Override
+    public CacheService getValue() throws IllegalStateException, IllegalArgumentException {
+        return this;
+    }
 
-	@Override
-	public void start(StartContext context) throws StartException {
-		try {
-			InitialContext ic = new InitialContext();
-			container = (CacheContainer) ic.lookup(this.infinispanServiceName());
-			if (container != null) {
-				container.start();
-			} else {
-				System.err.println("ERROR: Can't find ye olde cache container");
-			}
-		} catch (NamingException e) {
-			// TODO
-			System.err.println("ERROR: Cannot get cache container.");
-		}
-	}
+    @Override
+    public void start(StartContext context) throws StartException {
+        try {
+            InitialContext ic = new InitialContext();
+            container = (CacheContainer) ic.lookup( this.infinispanServiceName() );
+            if (container != null) {
+                container.start();
+            } else {
+                System.err.println( "ERROR: Can't find ye olde cache container" );
+            }
+        } catch (NamingException e) {
+            // TODO
+            System.err.println( "ERROR: Cannot get cache container." );
+        }
+    }
 
-	@Override
-	public void stop(StopContext context) {
-		// NOT SURE ABOUT THIS
-		// It seems like the service can be stopped while there are still apps that
-		// depend on it. So...
-		// if (container != null) { container.stop(); }
-	}
-	
-	public CacheContainer getCacheContainer() {
-		return this.container;
-	}
+    @Override
+    public void stop(StopContext context) {
+        // NOT SURE ABOUT THIS
+        // It seems like the service can be stopped while there are still apps
+        // that
+        // depend on it. So...
+        // if (container != null) { container.stop(); }
+    }
 
-	public void setClustered(boolean clustered) {
-		this.clustered  = clustered;
-	}
-	
-	public boolean isClustered() {
-		return this.clustered;
-	}
-	
-	private String infinispanServiceName() {
-		if (this.isClustered()) {
-			return "java:jboss/infinispan/container/web";
-		} else {
-			return "java:jboss/infinispan/container/polyglot";
-		}
-	}
+    public CacheContainer getCacheContainer() {
+        return this.container;
+    }
+
+    public void setClustered(boolean clustered) {
+        this.clustered = clustered;
+    }
+
+    public boolean isClustered() {
+        return this.clustered;
+    }
+
+    private String infinispanServiceName() {
+        if (this.isClustered()) {
+            return "java:jboss/infinispan/container/web";
+        } else {
+            return "java:jboss/infinispan/container/polyglot";
+        }
+    }
 
 }
