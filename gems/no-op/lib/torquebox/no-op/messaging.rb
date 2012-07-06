801200af829f42a5caa5b2dcb7aa0e3ff4e47bba
Dominique Broeglin <dominique.broeglin@gmail.com>
2012-07-06 08:25:40 +0200

    Added a no-op Torquebox::Messaging::Message class

--- gems/no-op/lib/torquebox/no-op/messaging.rb
+++ gems/no-op/lib/torquebox/no-op/messaging.rb
@@ -18,14 +18,22 @@ module TorqueBox
 
     class MessageProcessor
     end
-    
+
     class Queue
       def publish(*args)
         # no-op
       end
-      
+
       def receive(*args)
         # no-op
+      end
+    end
+
+    class Message
+      class <<self
+        def register_encoding(klass)
+          # no-op
+        end
       end
     end
   end
