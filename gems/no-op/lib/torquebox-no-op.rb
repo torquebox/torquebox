cb63fd04a979537f87c70e31c095c6f414156471
Dominique Broeglin <dominique.broeglin@gmail.com>
2012-08-03 15:55:12 +0200

    Added Torquebox.transaction

--- gems/no-op/lib/torquebox-no-op.rb
+++ gems/no-op/lib/torquebox-no-op.rb
@@ -1,3 +1,9 @@
 require "torquebox/no-op/messaging"
 require "torquebox/no-op/stomp"
 require "torquebox/no-op/injectors"
+
+module TorqueBox
+  def self.transaction(*args)
+    yield if block_given?
+  end
+end
