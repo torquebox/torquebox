f7d69216c51fd75ebe5542aa94bd6627d8c831b5
Ben Browning <bbrownin@redhat.com>
2012-09-13 18:33:49 -0400

    Properly set multi-value HTTP headers under Ruby 1.9 mode (TORQUE-937)
    Rack separates multiple values for the same header by newlines and
    it's up to us to separate this out into multiple response header
    entries all with the same key and the varying values. We were
    accidentally doing this when running under Ruby 1.8 mode but failing
    to do this under 1.9.

--- integration-tests/apps/rails3.2/basic/app/controllers/root_controller.rb
+++ integration-tests/apps/rails3.2/basic/app/controllers/root_controller.rb
@@ -13,4 +13,11 @@ class RootController < ApplicationController
     render :page_caching
   end
 
+  def multiple_cookies
+    cookies['foo1'] = 'bar1'
+    cookies['foo2'] = { :value => 'bar2', :expires => 1.hour.from_now }
+    cookies['foo3'] = 'bar3'
+    render :text => "something\n"
+  end
+
 end
