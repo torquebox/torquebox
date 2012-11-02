3f7481cc1ddd533c8a5e16d88119afba2f040b77
Ben Browning <bbrownin@redhat.com>
2012-11-02 12:46:54 -0400

    Read the files in chunks when generating SHA1s instead of all at once

--- build/dist/bin/generate_sha1.rb
+++ build/dist/bin/generate_sha1.rb
@@ -1,7 +1,12 @@
 require 'digest/sha1'
 
-Dir.glob( ARGV.first ) do |file|
+Dir.glob(ARGV.first) do |file|
   puts "Generating sha1 of: " + file
-  data = File.open(file, 'r') { |f| f.read }
-  File.open(file + '.sha1', 'w') { |f| f.write(Digest::SHA1.hexdigest(data)) }
+  sha1 = Digest::SHA1.new
+  File.open(file, 'r') do |f|
+    until f.eof?
+      sha1.update(f.read(16384))
+    end
+  end
+  File.open(file + '.sha1', 'w') { |f| f.write(sha1.hexdigest) }
 end
