c4d686f13575edad1d15aabf31a8fe4a4c08c0a8
Troels Parbst <twinsen@ubuntu.(none)>
2013-01-03 23:49:51 +0100

    Implemented sendfile filter and unit tests

---
+++ modules/web/src/test/java/org/torquebox/web/servlet/SendfileFilterTest.java
@@ -0,0 +1,11 @@
+package org.torquebox.web.servlet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twinsen
+ * Date: 12/31/12
+ * Time: 3:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SendfileFilterTest {
+}
