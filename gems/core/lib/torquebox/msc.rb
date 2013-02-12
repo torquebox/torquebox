a6c77863a5e208af39449dfffb6a1eeb492a435c
Ben Browning <bbrownin@redhat.com>
2013-02-12 09:44:14 -0500

    Let ServiceName handle quoting and scaping of parts for us

--- gems/core/lib/torquebox/msc.rb
+++ gems/core/lib/torquebox/msc.rb
@@ -71,11 +71,10 @@ module TorqueBox
         return nil if war_meta_data.nil? # no web component in this application
         jboss_web_meta_data = war_meta_data.getMergedJBossWebMetaData
         virtual_host = jboss_web_meta_data.virtual_hosts.first || 'default-host'
-        virtual_host = %Q("#{virtual_host}") if virtual_host.include?('.')
         context_path = jboss_web_meta_data.context_root
         context_path = "/#{context_path}" unless context_path.start_with?('/')
-        service_string = "jboss.web.deployment.#{virtual_host}.#{context_path}"
-        service_name = org.jboss.msc.service.ServiceName.parse(service_string)
+        deployment_service_name = org.jboss.msc.service.ServiceName.parse("jboss.web.deployment")
+        service_name = deployment_service_name.append(virtual_host).append(context_path)
         get_service(service_name).value
       end
     end
