c642b394a5aac2fd7e806e53013ba8bcfc7ee6a6
Jim Crossley <jim@crossleys.org>
2013-02-22 17:01:16 -0500

    Avoid a potential race condition here

--- integration-tests/apps/rails3.2/shared_cache/app/services/refresh_message.rb
+++ integration-tests/apps/rails3.2/shared_cache/app/services/refresh_message.rb
@@ -6,23 +6,15 @@ class RefreshMessage
   def initialize(opts = {})
     @persisted_cache = TorqueBox::Infinispan::Cache.new( :name => 'persisted_cache', :persist => true )
     @memory_cache    = TorqueBox::Infinispan::Cache.new( :name => 'memory_cache' )
-    @should_run      = true
   end
 
   def start
-    Thread.new { run }
+    persisted_cache.put( "time", Time.now )
+    memory_cache.put( "time", Time.now )
   end
 
   def stop
-    should_run = false
-  end
-  
-  def run
-    while( should_run ) do
-      persisted_cache.put( "time", Time.now )
-      memory_cache.put( "time", Time.now )
-      sleep 1
-    end
+    puts "Service stopped"
   end
   
 end
