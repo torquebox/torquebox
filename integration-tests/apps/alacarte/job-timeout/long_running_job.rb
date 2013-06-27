e9636e08b14472f5bfc9c6d63e22f289483e59b6
Ben Browning <bbrownin@redhat.com>
2013-06-27 11:51:05 -0400

    Use a CountDownLatch instead of sleep for jobs_timeout_spec

--- integration-tests/apps/alacarte/job-timeout/long_running_job.rb
+++ integration-tests/apps/alacarte/job-timeout/long_running_job.rb
@@ -3,20 +3,19 @@ class LongRunningJob
   def initialize(opts)
     @queue_name = opts['queue']
     @response_queue = TorqueBox::Messaging::Queue.new( @queue_name )
-    @interrupted = false
-
+    @latch = java.util.concurrent.CountDownLatch.new(1)
   end
 
   def run()
     $stderr.puts "Job executing! " + @queue_name
     @response_queue.publish( 'started' )
-    sleep( 5 )
-    @response_queue.publish( @interrupted ? 'interrupted' : 'done', :properties => { 'completion' => 'true' } )
+    interrupted = @latch.await(30, java.util.concurrent.TimeUnit::SECONDS)
+    @response_queue.publish( interrupted ? 'interrupted' : 'done', :properties => { 'completion' => 'true' } )
   end
 
   def on_timeout()
     $stderr.puts "Job was interrupted " + @queue_name
-    @interrupted = true
+    @latch.count_down
   end
 
 end
