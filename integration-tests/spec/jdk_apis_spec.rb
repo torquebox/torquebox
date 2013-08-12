369a5eca8d01b4b77cbf4b1023aad10270aac909
Ben Browning <bbrownin@redhat.com>
2013-08-12 17:31:12 -0400

    Ensure sun.jdk (and ibm.jdk) modules are available (TORQUE-1136)

--- integration-tests/spec/jdk_apis_spec.rb
+++ integration-tests/spec/jdk_apis_spec.rb
@@ -8,4 +8,15 @@ remote_describe 'jdk apis' do
       javax.transaction.Transaction
     }.should_not raise_error
   end
+
+  # Check on the client-side if we're using a Sun JDK
+  # before running any of those tests server-side
+  sun_jdk = org.jruby.util.unsafe.UnsafeGetter.getUnsafe rescue nil
+  if sun_jdk
+    it "should have sun.jdk modules available" do
+      lambda {
+        org.jruby.util.unsafe.UnsafeGetter.getUnsafe
+      }.should_not raise_error
+    end
+  end
 end
