5da1699e086b5eb0e512b3873a8d61668519655a
Ben Browning <bbrownin@redhat.com>
2014-01-09 17:05:46 -0500

    Update to JRuby 1.7.10 (TORQUE-1193)

--- modules/web/src/main/java/org/torquebox/web/rails/v2_3/gem_hack.rb
+++ modules/web/src/main/java/org/torquebox/web/rails/v2_3/gem_hack.rb
@@ -1,5 +1,7 @@
-# Rails 2 and RubyGems shipped with JRuby 1.7.5 don't play together
-if JRUBY_VERSION >= '1.7.5'
+# Rails 2 and RubyGems shipped with >= JRuby 1.7.5 don't play together
+versions = JRUBY_VERSION.split('.')[0,3]
+if versions.length == 3 && versions[0].to_i >= 1 && versions[1].to_i >= 7 &&
+    versions[2].to_i >= 5
   require 'rubygems'
   module Gem
     def self.source_index
