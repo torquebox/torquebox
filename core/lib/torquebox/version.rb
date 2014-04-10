050f2fe6447d178190225bb29430a072934e3ab4
Toby Crawley <toby@tcrawley.org>
2014-04-10 14:03:35 -0400

    Use wunderboss incrementals.

--- core/lib/torquebox/version.rb
+++ core/lib/torquebox/version.rb
@@ -17,5 +17,6 @@
 
 module TorqueBox
   VERSION = '4.0.0.alpha1.dev'
-  WUNDERBOSS_VERSION = '0.1.0-SNAPSHOT'
+  WUNDERBOSS_VERSION = '1.x.incremental.8'
+  #WUNDERBOSS_VERSION = '0.1.0-SNAPSHOT'
 end
