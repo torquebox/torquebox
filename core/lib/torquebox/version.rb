76d6d332d43b42e1e5921fd9710753d7839086d1
Toby Crawley <toby@tcrawley.org>
2014-07-13 09:15:58 -0600

    Update to newer wunderboss that uses the new provider/language discovery mechanism.
    See
    https://github.com/projectodd/wunderboss/commit/f6250bf65f5d3afad1ae228770d53bbbe459246a
    for more details.

--- core/lib/torquebox/version.rb
+++ core/lib/torquebox/version.rb
@@ -14,7 +14,7 @@
 
 module TorqueBox
   VERSION = '4.0.0.alpha1.dev'
-  WUNDERBOSS_VERSION = '1.x.incremental.89'
+  WUNDERBOSS_VERSION = '1.x.incremental.90'
   #WUNDERBOSS_VERSION = '0.1.0-SNAPSHOT'
   WILDFLY_VERSION = '8.1.0.Final'
 end
