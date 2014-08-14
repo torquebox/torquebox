c7c4de222d2c2be357493a41b78bbbf92a94df12
Ben Browning <bbrownin@redhat.com>
2014-08-14 15:47:22 -0400

    Revert "Grasping at straws on Travis at this point"
    This reverts commit fc19d924ca1bc099023b9a87d78d7759038653b9.

--- integration-tests/spec/spec_helper.rb
+++ integration-tests/spec/spec_helper.rb
@@ -205,7 +205,7 @@ def torquebox(options)
       else
         command << " jar -v"
       end
-      jar_output = `#{command} 2>&1`
+      jar_output = `#{command}`
       puts jar_output if ENV['DEBUG']
       puts `jar -tf #{jarfile} | grep -i torquebox` if ENV['DEBUG']
       $wildfly.deploy(jarfile) if wildfly?
