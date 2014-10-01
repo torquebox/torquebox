e8451846a7c9a9625f1c06e89c0af6d707f400e3
Toby Crawley <toby@tcrawley.org>
2014-10-01 17:49:51 -0400

    Update to the latest wunderboss version.

--- core/lib/torquebox/version.rb
+++ core/lib/torquebox/version.rb
@@ -15,7 +15,7 @@
 
 module TorqueBox
   VERSION = '4.0.0.alpha1.dev'
-  WUNDERBOSS_VERSION = '1.x.incremental.120'
+  WUNDERBOSS_VERSION = '1.x.incremental.133'
   # WUNDERBOSS_VERSION = '0.2.0-SNAPSHOT'
   WILDFLY_VERSION = '8.1.0.Final'
 end
