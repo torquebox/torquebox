1cc085d75be2843fa908f4bee2901a63d2316085
Ben Browning <bbrownin@redhat.com>
2014-10-20 16:52:43 -0400

    Don't mark Rack responses as committed if using X-Sendfile (TORQUE-1240)
    For sendfile support to work properly, we have to ensure any responses
    using it don't mark the response as committed. But, for all other
    responses, we do want to make the response as committed to prevent
    recreating the issue fixed in TORQUE-1215.

--- modules/web/src/main/java/org/torquebox/web/rack/response_handler.rb
+++ modules/web/src/main/java/org/torquebox/web/rack/response_handler.rb
@@ -50,7 +50,9 @@ module TorqueBox
           else
             out.write( body.to_java_bytes )
           end
-          servlet_response.flush_buffer
+          unless headers.fetch('X-Sendfile', nil)
+            servlet_response.flush_buffer
+          end
         rescue NativeException => e
           # Don't needlessly raise errors because of client abort exceptions
           raise unless e.cause.toString =~ /(clientabortexception|broken pipe)/i
