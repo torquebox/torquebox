bbfa31f97a04abb2ac374c9b612827297daba257
Ben Browning <bbrownin@redhat.com>
2014-11-24 09:59:39 -0500

    Bump to wunderboss 0.3.0

--- core/lib/torquebox/version.rb
+++ core/lib/torquebox/version.rb
@@ -15,8 +15,6 @@
 
 module TorqueBox
   VERSION = '4.0.0.alpha1.dev'
-  WUNDERBOSS_VERSION = '1.x.incremental.169'
-  # WUNDERBOSS_VERSION = '0.2.0'
-  # WUNDERBOSS_VERSION = '0.3.0-SNAPSHOT'
+  WUNDERBOSS_VERSION = '0.3.0'
   WILDFLY_VERSION = '8.2.0.Final'
 end
