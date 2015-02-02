f2ee0ad757dd182778b559ffb3204a021add03cd
Toby Crawley <toby@tcrawley.org>
2015-02-02 14:57:58 -0500

    Fix table rendering in HornetQ docstring.

--- messaging/lib/torquebox/messaging/hornetq.rb
+++ messaging/lib/torquebox/messaging/hornetq.rb
@@ -24,19 +24,17 @@ module TorqueBox
       # The returned controller depends on the type of the given
       # destination and, for queues, the requested type:
       #
-      # ```
-      # destination  type          controller
-      # -------------------------------------------------------------------------
-      # Queue        :jms          org.hornetq.api.jms.management.JMSQueueControl
-      # Queue        :core         org.hornetq.api.core.management.QueueControl
-      # Topic        <ignored>     org.hornetq.api.jms.management.TopicControl
-      # ```
+      #     destination  type          controller
+      #     -------------------------------------------------------------------------
+      #     Queue        :jms          org.hornetq.api.jms.management.JMSQueueControl
+      #     Queue        :core         org.hornetq.api.core.management.QueueControl
+      #     Topic        <ignored>     org.hornetq.api.jms.management.TopicControl
       #
       # Refer to the javadocs for those control classes for details on the
       # available operations.
       # @param destination [Destination] Should be a {Queue} or
       #   {Topic}.
-      # @param type [:core, :jms]
+      # @param type [:core, :jms]"
       def self.destination_controller(destination, type = :jms)
         if destination.instance_of?(Queue) && type == :core
           prefix = "core.queue."
