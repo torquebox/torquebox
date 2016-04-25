263d3f3bb35c22211f3fab3d7d340b4fc12cbf79
Michael Zaccari <michael.zaccari@accelerated.com>
2016-04-25 11:34:46 -0400

    Add class for running shell commands
    This is a simple wrapper for running a shell command and capturing the
    output & exit code. If anything more complex is needed then a gem like
    `childprocess` might be a better fit:
    
      https://github.com/jarib/childprocess

---
+++ core/lib/torquebox/cli/shell_command.rb
@@ -0,0 +1,114 @@
+# Copyright 2016 Red Hat, Inc, and individual contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module TorqueBox
+  class CLI
+    class ShellCommand
+
+      EXIT_SUCCESS = 0
+
+      attr_reader :command, :options, :prefix, :output, :exit_code
+
+      def initialize(command, options = {})
+        @command   = command
+        @options   = options
+        @prefix    = options[:prefix]
+        @output    = nil
+        @exit_code = nil
+      end
+
+      def run
+        with_blank_rubyopt do
+          begin
+            run_command
+          rescue => e
+            print_error(e)
+          end
+        end
+        self
+      end
+
+      def succeeded?
+        run if exit_code.nil?
+        exit_code == EXIT_SUCCESS
+      end
+
+      def failed?
+        !succeeded?
+      end
+
+      def to_s
+        @output.to_s
+      end
+
+      class << self
+        def run(command, options = {})
+          new(command, options).run
+        end
+
+        def run_jruby(command, options = {})
+          run(command, options.merge(prefix: jruby_prefix))
+        end
+
+        def jruby_prefix
+          @jruby_prefix ||= [jruby_path, jruby_cli_version, '-S'].join(' ')
+        end
+
+        def jruby_path
+          @jruby_path ||= begin
+            File.join(
+              RbConfig::CONFIG['bindir'],
+              RbConfig::CONFIG['ruby_install_name']
+            )
+          end
+        end
+
+        def jruby_cli_version
+          @jruby_cli_version ||= begin
+            unless JRUBY_VERSION >= '9'
+              case RUBY_VERSION
+              when /^1\.8\./ then '--1.8'
+              when /^1\.9\./ then '--1.9'
+              when /^2\.0\./ then '--2.0'
+              end
+            end
+          end
+        end
+      end
+
+      private
+
+      def with_blank_rubyopt
+        old_rubyopt = ENV['RUBYOPT']
+        begin
+          ENV['RUBYOPT'] = ''
+          yield if block_given?
+        ensure
+          ENV['RUBYOPT'] = old_rubyopt
+        end
+      end
+
+      def run_command
+        @output    = %x[ #{prefix} #{command} ]
+        @exit_code = $?.exitstatus
+        puts @output
+      end
+
+      def print_error(error)
+        puts error.message
+        puts error.backtrace.join("\n")
+      end
+    end
+  end
+end
---
+++ core/spec/shell_command_spec.rb
@@ -0,0 +1,128 @@
+# Copyright 2014 Red Hat, Inc, and individual contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+
+describe TorqueBox::CLI::ShellCommand do
+
+  let(:command)       { 'echo "test"' }
+  let(:shell_command) { TorqueBox::CLI::ShellCommand.new(command) }
+
+  describe '#run' do
+    it 'runs the command' do
+      shell_command.run
+      expect(shell_command.output.strip).to eq 'test'
+    end
+
+    it 'sets the exit code' do
+      expect {
+        shell_command.run
+      }.to change { shell_command.exit_code }.from(nil).to(0)
+    end
+
+    it 'returns a reference to the shell command object' do
+      expect(shell_command.run).to be_a TorqueBox::CLI::ShellCommand
+    end
+
+    context 'when the RUBYOPT environment variable is set' do
+      let(:command) { 'echo $RUBYOPT' }
+
+      before do
+        @old_rubyopt = ENV['RUBYOPT']
+        ENV['RUBYOPT'] = 'test'
+      end
+
+      after do
+        ENV['RUBYOPT'] = @old_rubyopt
+      end
+
+      it 'clears the RUBYOPT environment variable' do
+        shell_command.run
+        expect(shell_command.output.strip).to be_empty
+      end
+    end
+
+    context 'when the command throws an exception' do
+      before do
+        allow(shell_command).to receive(:run_command).and_raise('error')
+      end
+
+      it 'prints the error' do
+        expect(shell_command).to receive(:print_error)
+        shell_command.run
+      end
+    end
+  end
+
+  describe '#succeeded?' do
+    context 'when the command has already run' do
+      before do
+        shell_command.run
+      end
+
+      it 'does not re-run the command' do
+        expect(shell_command).to_not receive(:run)
+        shell_command.succeeded?
+      end
+    end
+
+    context 'when the command has not run yet' do
+      it 'runs the command' do
+        expect(shell_command).to receive(:run)
+        shell_command.succeeded?
+      end
+    end
+
+    context 'when the command succeeds' do
+      let(:command) { 'true' }
+
+      it 'returns true' do
+        expect(shell_command.succeeded?).to be true
+      end
+    end
+
+    context 'when the command fails' do
+      let(:command) { 'false' }
+
+      it 'returns false' do
+        expect(shell_command.succeeded?).to be false
+      end
+    end
+  end
+
+  describe '#failed?' do
+    context 'when the command succeeds' do
+      let(:command) { 'true' }
+
+      it 'returns false' do
+        expect(shell_command.failed?).to be false
+      end
+    end
+
+    context 'when the command fails' do
+      let(:command) { 'false' }
+
+      it 'returns true' do
+        expect(shell_command.failed?).to be true
+      end
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns the output as a string' do
+      shell_command.run
+      expect(shell_command.to_s.strip).to eq 'test'
+    end
+  end
+end
